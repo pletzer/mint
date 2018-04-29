e1f6d45f19c026d2d779fd210630995d6f7e74cb
Alex Pletzer <alexander@gokliya.net>
2018-04-29 14:00:42 +1200

    + constructing grid

--- data/cubedsphere_reader.py
+++ data/cubedsphere_reader.py
@@ -8,19 +8,55 @@ class CubedsphereReader:
         
         nc = netCDF4.Dataset(filename, 'r')
 
-        self.longitude = []
-        self.latitude = []
         self.cell_connectivity = []
 
+        lats, lons = None, None
         for varname in nc.variables:
             var = nc.variables[varname]
             if hasattr(var, 'cf_role') and var.cf_role == 'face_node_connectivity':
                 self.cell_connectivity = var[:]
             elif hasattr(var, 'standard_name'):
                 if var.standard_name == 'longitude':
-                    self.longitude = var[:]
+                    lons = var[:]
                 elif var.standard_name == 'latitude':
-                    self.latitude = var[:]
+                    lats = var[:]
+
+        nverts = len(lons)
+        self.xyz = numpy.zeros((nverts, 3), numpy.float64)
+        self.xyz[:, 0] = lats
+        self.xyz[:, 1] = lons
+
+        ncells = self.cell_connectivity.shape[0]
+        # use zero-based indexing from now on
+        self.cell_connectivity -= 1
+
+        # build unstructured grid
+        self.vtk = {
+            'point_array': vtk.vtkDoubleArray(),
+            'points': vtk.vtkPoints(),
+            'cell_array': vtk.vtkIntArray(),
+            'cells': vtk.vtkCellArray(),
+            'grid': vtk.vtkUnstructuredGrid()
+        }
+
+        self.vtk['point_array'].SetNumberOfComponents(3)
+        self.vtk['point_array'].SetNumberOfTuples(nverts)
+        self.vtk['point_array'].SetVoidArray(self.xyz, 3*nverts, 1)
+
+
+        self.vtk['points'].SetNumberOfPoints(nverts)
+        self.vtk['points'].SetData(self.vtk['point_array'])
+
+        self.vtk['cell_array'].SetNumberOfComponents(4)
+        self.vtk['cell_array'].SetNumberOfTuples(ncells)
+        self.vtk['cell_array'].SetVoidArray(self.cell_connectivity, 4*ncells, 1)
+
+        self.vtk['grid'].SetPoints(self.vtk['points'])
+        self.vtk['grid'].SetCells(vtk.VTK_QUAD, self.vtk['cells'])
+
+
+
+        
 
 
 
