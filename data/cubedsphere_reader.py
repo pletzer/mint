b5b49a7c01ba54c4663f9fc448bd7cf93511a9f0
Alex Pletzer <alexander@gokliya.net>
2018-04-29 16:30:57 +1200

    now creating u grid cell by cell

--- data/cubedsphere_reader.py
+++ data/cubedsphere_reader.py
@@ -10,6 +10,7 @@ class CubedsphereReader:
         @param filename UGRID file 
         """
         
+        # read UGRID file
         nc = netCDF4.Dataset(filename, 'r')
 
         self.cell_connectivity = []
@@ -26,47 +27,53 @@ class CubedsphereReader:
                 elif var.standard_name == 'latitude':
                     lats = var[:]
 
-        nverts = len(lons)
-        self.xyz = numpy.zeros((nverts, 3), numpy.float64)
-        self.xyz[:, 0] = lons
-        self.xyz[:, 1] = lats
-
         ncells = connectivity.shape[0]
 
-        self.connectivity = numpy.zeros((ncells, 5), numpy.int64)
-        self.connectivity[:, 0] = 4 # points per cell
-        self.connectivity[:, 1:] = connectivity - 1 # zero based
-
+        # construct the unstructured grid as a collection of 
+        # 2D cells. Each cell has its own cooordinates. Make
+        # sure each cell's area is positive in lat-lon space
         # build unstructured grid
         self.vtk = {
-            'point_array': vtk.vtkDoubleArray(),
             'points': vtk.vtkPoints(),
-            'cell_array': vtk.vtkIdTypeArray(),
-            'cells': vtk.vtkCellArray(),
             'grid': vtk.vtkUnstructuredGrid()
         }
 
-        self.vtk['point_array'].SetNumberOfComponents(3)
-        self.vtk['point_array'].SetNumberOfTuples(nverts)
-        self.vtk['point_array'].SetVoidArray(self.xyz, 3*nverts, 1)
+        self.vtk['points'].SetNumberOfPoints(4 * ncells)
 
+        self.vtk['grid'].Allocate(1, 1)
+        ptIds = vtk.vtkIdList()
+        ptIds.SetNumberOfIds(4)
+        for icell in range(ncells):
 
-        self.vtk['points'].SetNumberOfPoints(nverts)
-        self.vtk['points'].SetData(self.vtk['point_array'])
+            i00, i10, i11, i01 = connectivity[icell, :] - 1 # zero based indexing
 
-        self.vtk['cell_array'].SetNumberOfComponents(5)
-        self.vtk['cell_array'].SetNumberOfTuples(ncells)
-        self.vtk['cell_array'].SetVoidArray(self.connectivity, 5*ncells, 1)
+            lon00, lat00 = lons[i00], lats[i00]
+            lon10, lat10 = lons[i10], lats[i10]
+            lon11, lat11 = lons[i11], lats[i11]
+            lon01, lat01 = lons[i01], lats[i01]
 
-        self.vtk['cells'].SetNumberOfCells(ncells)
-        self.vtk['cells'].SetCells(ncells, self.vtk['cell_array'])
+            area013 = 0.5*( (lon10 - lon00)*(lat01 - lat00) - (lat10 - lat00)*(lon01 - lon00) )
+            area231 = 0.5*( (lon01 - lon11)*(lat10 - lat11) - (lat01 - lat11)*(lon10 - lon11) )
+
+            if area013 < 0.:
+                print '--- icell = ', icell, ' area013 = ', area013, ' lon-lats = ', (lon00, lat00), (lon10, lat10), (lon01, lat01)
+            if area231 < 0.:
+                print '--- icell = ', icell, ' area231 = ', area231, ' lon-lats = ', (lon11, lat11), (lon01, lat01), (lon10, lat10)
+
+            k = 4*icell
+
+            self.vtk['points'].InsertPoint(k + 0, lon00, lat00, 0.)
+            self.vtk['points'].InsertPoint(k + 1, lon10, lat10, 0.)
+            self.vtk['points'].InsertPoint(k + 2, lon11, lat11, 0.)
+            self.vtk['points'].InsertPoint(k + 3, lon01, lat01, 0.)
+
+            ptIds.SetId(0, k + 0)
+            ptIds.SetId(1, k + 1)
+            ptIds.SetId(2, k + 2)
+            ptIds.SetId(3, k + 3)
+            self.vtk['grid'].InsertNextCell(vtk.VTK_QUAD, ptIds)
 
         self.vtk['grid'].SetPoints(self.vtk['points'])
-
-        print self.vtk['cells']
-        self.vtk['grid'].Allocate(ncells, 1)
-        self.vtk['grid'].SetCells(vtk.VTK_QUAD, self.vtk['cells'])
-
 
         
     def saveToVtkFile(self, filename):
