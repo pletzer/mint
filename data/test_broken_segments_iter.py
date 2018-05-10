11eaf29007b005abc79ff64b3c5787d5cc637fa5
Alexander Pletzer <pletzera@niwa-1019500.niwa.local>
2018-05-11 11:24:45 +1200

    more tests

--- data/test_broken_segments_iter.py
+++ data/test_broken_segments_iter.py
@@ -1,5 +1,6 @@
 from math import pi
 from ugrid_reader import UgridReader
+from latlon_reader import LatLonReader
 from broken_line_iter import BrokenLineIter
 from broken_segments_iter import BrokenSegmentsIter
 
@@ -25,8 +26,33 @@ def test(filename, points):
     print('Integrated t = {}'.format(totalT))
     assert(abs(totalT - 1.0) < 1.e-10)
 
+def testUm(filename, points, padding):
+
+    ur = LatLonReader(filename=filename, padding=padding)
+    bl = BrokenLineIter(points)
+    bs = BrokenSegmentsIter(ur.getUnstructuredGrid(), 
+                            ur.getUnstructuredGridCellLocator(), 
+                            bl)
+    count = 0
+    for s in bs:
+        cellId = s.getCellId()
+        xia = s.getBegCellParamCoord()
+        xib = s.getEndCellParamCoord()
+        ta = s.getBegLineParamCoord()
+        tb = s.getEndLineParamCoord()
+        coeff = s.getCoefficient()
+        print('seg {} in cell {} t = {} -> {} xi = {} -> {} coeff = {}'.format(count, cellId, ta, tb, xia, xib, coeff))
+        count += 1
+
+    totalT = s.getIntegratedParamCoord()
+    print('Integrated t = {}'.format(totalT))
+    assert(abs(totalT - 1.0) < 1.e-10)
+
+
 
 if __name__ == '__main__':
+
+    # UGRID data
     print('Test0 ' + '-'*40)
     eps = 1.73654365e-10
     test(filename='mesh_C4.nc', points=[(pi/2., 0.-eps, 0.),(pi/2. + 2.*pi/16., 0.+eps, 0.)])
@@ -54,6 +80,12 @@ if __name__ == '__main__':
     test(filename='mesh_C4.nc', points=[(0.0, 1.1780972450961724, 0.0), (0.7853981633974483, 1.04089353704597, 0.0)])
     print('Test12' + '-'*40)
     test(filename='mesh_C4.nc', points=[(3.9269908169872414, 1.04089353704597, 0.0), (3.141592653589793, 1.1780972450961724, 0.0)])
+
+    # UM grid
+    print('Test13' + '-'*40)
+    testUm(filename='um10x4.nc', points=[(6.283185307179586, 1.1780972450961724, 0.0), (7.853981633974483, 1.5707963267948966, 0.0)], padding=4)
+    print('Test14' + '-'*40)
+    testUm(filename='um10x4.nc', points=[(7.853981633974483, 1.5707963267948966, 0.0), (4.71238898038469, 1.1780972450961724, 0.0)], padding=4)
 
 
 
