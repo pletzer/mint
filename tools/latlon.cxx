b839d6bb596b78085cfefd46926f6c31de8564c6
Alexander Pletzer <pletzera@niwa-1019500.home>
2018-05-30 16:05:16 +0200

    work in progress

--- tools/latlon.cxx
+++ tools/latlon.cxx
@@ -1,5 +1,6 @@
 #include <mntLatLon.h>
 #include <CmdLineArgParser.h>
+#include <iostream>
 
 int main(int argc, char** argv) {
 
@@ -9,10 +10,27 @@ int main(int argc, char** argv) {
     args.set("-nlon", 4, "Number of longitude cells");
     args.set("-o", std::string("um.nc"), "Output file");
 
-    int nlat = args.get<int>("-nlat");
-    int nlon = args.get<int>("-nlon");
+    bool success = args.parse(argc, argv);
+    bool help = args.get<bool>("-h");
 
-    mntLatLon_t *ll;
-    mnt_latlon_new(&ll);
-    mnt_latlon_del(&ll);
+    if (success && !help) {
+        int nlat = args.get<int>("-nlat");
+        int nlon = args.get<int>("-nlon");
+
+        mntLatLon_t *ll;
+        mnt_latlon_new(&ll);
+        mnt_latlon_setNumberOfLatCells(&ll, (size_t) args.get<int>("-nlat"));
+        mnt_latlon_setNumberOfLonCells(&ll, (size_t) args.get<int>("-nlon"));
+        mnt_latlon_build(&ll);
+        mnt_latlon_dump(&ll, args.get<std::string>("-o"));
+        mnt_latlon_del(&ll);
+    }
+    else if (help) {
+        args.help();
+    }
+    else {
+        std::cerr << "ERROR when parsing command line arguments\n";
+    }
+
+    return 0;
 }
