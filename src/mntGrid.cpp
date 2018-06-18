ca4ec20863c53791dea14481c700480c06c0f878
Alexander Pletzer <pletzera@niwa-1010022.niwa.local>
2018-06-18 22:01:02 +0100

    mntGrid: separating prototypes from implementation

---
+++ src/mntGrid.cpp
@@ -0,0 +1,86 @@
+#include <mntGrid.h>
+
+extern "C" 
+int mnt_grid_new(Grid_t** self) {
+    *self = new Grid_t();
+    (*self)->pointData = NULL;
+    (*self)->points = NULL;
+    (*self)->grid = NULL;
+    (*self)->reader = NULL;
+    (*self)->writer = NULL;
+    return 0;
+}
+
+extern "C"
+int mnt_grid_del(Grid_t** self) {
+    if ((*self)->writer) (*self)->writer->Delete();
+    if ((*self)->reader) {
+        (*self)->reader->Delete();
+    }
+    else {
+        if ((*self)->grid) (*self)->grid->Delete();
+    }
+    if ((*self)->points) (*self)->points->Delete();
+    if ((*self)->pointData) (*self)->pointData->Delete();
+    delete *self;
+    return 0;
+}
+
+extern "C"
+int mnt_grid_setpoints(Grid_t** self, int ncells, const double points[]) {
+
+    (*self)->pointData = vtkDoubleArray::New();
+    (*self)->points = vtkPoints::New();
+    (*self)->grid = vtkUnstructuredGrid::New();
+
+    int save = 1;
+    (*self)->pointData->SetNumberOfComponents(3);
+    int npoints = 4 * ncells;
+    (*self)->pointData->SetNumberOfTuples(npoints);
+    (*self)->pointData->SetVoidArray((double*) points, npoints, save);
+
+    (*self)->points->SetData((*self)->pointData);
+
+    (*self)->grid->Allocate(ncells, 1);
+    (*self)->grid->SetPoints((*self)->points);
+
+    // connect
+    vtkIdList* ptIds = vtkIdList::New();
+    ptIds->SetNumberOfIds(4); // quad
+    for (int i = 0; i < ncells; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            ptIds->SetId(j, 4*i + j);
+        }
+        (*self)->grid->InsertNextCell(VTK_QUAD, ptIds);
+    }
+    ptIds->Delete();
+
+    return 0;
+}
+
+extern "C"
+int mnt_grid_get(Grid_t** self, vtkUnstructuredGrid** grid_ptr) {
+    *grid_ptr = (*self)->grid;
+    return 0;
+}
+
+extern "C"
+int mnt_grid_load(Grid_t** self, const char* filename) {
+    if ((*self)->grid) {
+        (*self)->grid->Delete();
+    }
+    (*self)->reader = vtkUnstructuredGridReader::New();
+    (*self)->reader->SetFileName(filename);
+    (*self)->reader->Update();
+    (*self)->grid = (*self)->reader->GetOutput();
+    return 0;
+}
+
+extern "C"
+int mnt_grid_dump(Grid_t** self, const char* filename) {
+    (*self)->writer = vtkUnstructuredGridWriter::New();
+    (*self)->writer->SetFileName(filename);
+    (*self)->writer->SetInputData((*self)->grid);
+    (*self)->writer->Update();
+    return 0;
+}
--- src/mntGrid.h
+++ src/mntGrid.h
@@ -24,15 +24,7 @@ struct Grid_t {
  * @return error code (0 = OK)
  */
 extern "C" 
-int mnt_grid_new(Grid_t** self) {
-    *self = new Grid_t();
-    (*self)->pointData = NULL;
-    (*self)->points = NULL;
-    (*self)->grid = NULL;
-    (*self)->reader = NULL;
-    (*self)->writer = NULL;
-    return 0;
-}
+int mnt_grid_new(Grid_t** self);
 
 /**
  * Destructor
@@ -40,19 +32,7 @@ int mnt_grid_new(Grid_t** self) {
  * @return error code (0 = OK)
  */
 extern "C"
-int mnt_grid_del(Grid_t** self) {
-    if ((*self)->writer) (*self)->writer->Delete();
-    if ((*self)->reader) {
-        (*self)->reader->Delete();
-    }
-    else {
-        if ((*self)->grid) (*self)->grid->Delete();
-    }
-    if ((*self)->points) (*self)->points->Delete();
-    if ((*self)->pointData) (*self)->pointData->Delete();
-    delete *self;
-    return 0;
-}
+int mnt_grid_del(Grid_t** self);
 
 /**
  * Set points
@@ -62,36 +42,7 @@ int mnt_grid_del(Grid_t** self) {
  * @return error code (0 = OK)
  */
 extern "C"
-int mnt_grid_setpoints(Grid_t** self, int ncells, const double points[]) {
-
-    (*self)->pointData = vtkDoubleArray::New();
-    (*self)->points = vtkPoints::New();
-    (*self)->grid = vtkUnstructuredGrid::New();
-
-    int save = 1;
-    (*self)->pointData->SetNumberOfComponents(3);
-    int npoints = 4 * ncells;
-    (*self)->pointData->SetNumberOfTuples(npoints);
-    (*self)->pointData->SetVoidArray((double*) points, npoints, save);
-
-    (*self)->points->SetData((*self)->pointData);
-
-    (*self)->grid->Allocate(ncells, 1);
-    (*self)->grid->SetPoints((*self)->points);
-
-    // connect
-    vtkIdList* ptIds = vtkIdList::New();
-    ptIds->SetNumberOfIds(4); // quad
-    for (int i = 0; i < ncells; ++i) {
-        for (int j = 0; j < 4; ++j) {
-            ptIds->SetId(j, 4*i + j);
-        }
-        (*self)->grid->InsertNextCell(VTK_QUAD, ptIds);
-    }
-    ptIds->Delete();
-
-    return 0;
-}
+int mnt_grid_setpoints(Grid_t** self, int ncells, const double points[]);
 
 /**
  * Get the VTK unstructured grid
@@ -100,10 +51,7 @@ int mnt_grid_setpoints(Grid_t** self, int ncells, const double points[]) {
  * @return error code (0 = OK)
  */
 extern "C"
-int mnt_grid_get(Grid_t** self, vtkUnstructuredGrid** grid_ptr) {
-    *grid_ptr = (*self)->grid;
-    return 0;
-}
+int mnt_grid_get(Grid_t** self, vtkUnstructuredGrid** grid_ptr);
 
 /**
  * Load grid from file
@@ -113,16 +61,7 @@ int mnt_grid_get(Grid_t** self, vtkUnstructuredGrid** grid_ptr) {
  * @note user should invoke mnt_grid_del to free memory
  */
 extern "C"
-int mnt_grid_load(Grid_t** self, const char* filename) {
-    if ((*self)->grid) {
-        (*self)->grid->Delete();
-    }
-    (*self)->reader = vtkUnstructuredGridReader::New();
-    (*self)->reader->SetFileName(filename);
-    (*self)->reader->Update();
-    (*self)->grid = (*self)->reader->GetOutput();
-    return 0;
-}
+int mnt_grid_load(Grid_t** self, const char* filename);
 
 /**
  * Load grid to file
@@ -131,12 +70,6 @@ int mnt_grid_load(Grid_t** self, const char* filename) {
  * @return error code (0 = OK)
  */
 extern "C"
-int mnt_grid_dump(Grid_t** self, const char* filename) {
-    (*self)->writer = vtkUnstructuredGridWriter::New();
-    (*self)->writer->SetFileName(filename);
-    (*self)->writer->SetInputData((*self)->grid);
-    (*self)->writer->Update();
-    return 0;
-}
+int mnt_grid_dump(Grid_t** self, const char* filename);
 
 #endif // MNT_GRID
