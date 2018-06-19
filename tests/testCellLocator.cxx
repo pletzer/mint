eca20280923c1357fb6a227cebc7815c042cef7f
Alexander Pletzer <pletzera@NIWA-1019500.local>
2018-06-19 18:45:17 +0100

    ~ bug fix

--- tests/testCellLocator.cxx
+++ tests/testCellLocator.cxx
@@ -6,13 +6,14 @@ void test1() {
     CellLocator_t* cloc;
     mnt_celllocator_new(&cloc);
     int num_cells = 1;
+    int num_points = num_cells * 4;
     const double verts[] = {0., 0., 0.,
                             1., 0., 0.,
                             1., 1., 0.,
                             0., 1., 0.};
-    mnt_celllocator_setpoints(&cloc, num_cells, verts);
+    mnt_celllocator_setpoints(&cloc, num_points, verts);
     mnt_celllocator_build(&cloc);
-    const double target_point[] = {0.1, 0.2, 0.};
+    const double target_point[] = {0.2, 0.3, 0.};
     double pcoords[3];
     long long cell_id;
     mnt_celllocator_find(&cloc, target_point, &cell_id, pcoords);
