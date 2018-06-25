195f1bbf1d29c404ca19a7b6294ed68ab08704de
Alexander Pletzer <pletzera@niwa-39099.niwa.local>
2018-06-25 11:31:21 +0200

    work in progress

--- tools/stream_function_flux.cxx
+++ tools/stream_function_flux.cxx
@@ -5,6 +5,7 @@
 #include <CmdLineArgParser.h>
 #include <vtkUnstructuredGrid.h>
 #include <vtkCellLocator.h>
+#include <vtkCellData.h>
 #include <string>
 #include <iostream>
 #include <cstdio>
@@ -17,6 +18,7 @@ int main(int argc, char** argv) {
     CmdLineArgParser args;
     args.setPurpose("Project a line segment.");
     args.set("-i", std::string(""), "Source grid file in VTK format");
+    args.set("-v", std::string("edge_integrated_velocity"), "Specify edge staggered field variable name in VTK file");
     args.set("-xline", std::string("pi/4. + 0.2*cos(5*pi/3. - 0.2"), "Parametric x coordinate (lon in [rad]) expression of 0 <= t <= 1");
     args.set("-yline", std::string("pi/4. + 0.2*sin(5*pi/3. - 0.2"), "Parametric y coordinate (lat in [rad]) expression of 0 <= t <= 1");
     args.set("-nline", 2, "Number of points defining the line (>= 2)");
@@ -26,7 +28,11 @@ int main(int argc, char** argv) {
     bool help = args.get<bool>("-h");
 
     if (success && !help) {
+        
+        // get the variable name of the field to project
+        std::string varname = args.get<std::string>("-v");
 
+        // construct the projection line from the x and y expressions
         std::string xLineExpr = args.get<std::string>("-xline");
         std::string yLineExpr = args.get<std::string>("-yline");
         int nline = args.get<int>("-nline");
@@ -35,10 +41,12 @@ int main(int argc, char** argv) {
 
         GrExprParser xExpr(ts.size(), GrExprAdaptor(xLineExpr).getPrefixExpr());
         xExpr.defineVariable("t", &ts);
-        Vec* xs = xExpr.eval();
-
+        
         GrExprParser yExpr(ts.size(), GrExprAdaptor(yLineExpr).getPrefixExpr());
         yExpr.defineVariable("t", &ts);
+
+        // hold the line cooridnates
+        Vec* xs = xExpr.eval();
         Vec* ys = yExpr.eval();
 
         std::string srcFile = args.get<std::string>("-i");
@@ -63,10 +71,21 @@ int main(int argc, char** argv) {
 
         std::cout << "Grid: no of cells " << grid->GetNumberOfCells() << " no of points " << grid->GetNumberOfPoints() << '\n';
 
+        // get the velocity field
+        vtkCellData* cellData = grid->GetCellData();
+        vtkAbstractArray* aa = cellData->GetAbstractArray(varname.c_str());
+        if (!aa) {
+            std::cerr << "ERROR: must specify a a valid field to project (-v)\n";
+            return 3;
+        }
+        double* srcData = (double*) aa->GetVoidPointer(0);
+
         // build locator
         vtkCellLocator* loc = vtkCellLocator::New();
         loc->SetDataSet(grid);
         loc->BuildLocator();
+
+        double flux = 0.0;
 
         // iterate over segments
         for (size_t ip0 = 0; ip0 < ts.size() - 1; ++ip0) {
@@ -85,20 +104,43 @@ int main(int argc, char** argv) {
             for (size_t i = 0; i < numSubSegs; ++i) {
 
                 vtkIdType cellId = polyseg.getCellId();
+
                 double ta = polyseg.getBegLineParamCoord();
                 double tb = polyseg.getEndLineParamCoord();
                 const std::vector<double>& xia = polyseg.getBegCellParamCoord();
                 const std::vector<double>& xib = polyseg.getEndCellParamCoord();
                 double coeff = polyseg.getCoefficient();
 
+                std::vector<double> dxi({xib[0] - xia[0], xib[1] - xia[1]});
+                std::vector<double> xiMid({0.5*(xia[0] + xib[0]), 0.5*(xia[1] + xib[1])});
+
+                // compute the weight contributions from each src edge
+                double ws[] = {+ dxi[0] * (1.0 - xiMid[1]) * coeff,
+                               + dxi[1] * (0.0 + xiMid[0]) * coeff,
+                               - dxi[0] * (0.0 + xiMid[1]) * coeff,
+                               - dxi[1] * (1.0 - xiMid[0]) * coeff};
+
+                // project
+                for (size_t j = 0; j < 4; ++j) {
+                    flux += ws[0]*srcData[cellId*4 + j];
+                }
+
                 std::cout << "cell " << cellId << " t = " << ta  << " -> " << tb << 
                             " xi = " << xia[0] << ',' << xia[1] << " -> " << xib[0] << ',' << xib[1] << 
                             " coeff = " << coeff << '\n';
+
                 polyseg.next();
             }
+
             double tTotal = polyseg.getIntegratedParamCoord();
+            if (std::abs(tTotal - 1.0) > 1.e-10) {
+                std::cerr << "Warning: total t of segment " << ip0
+                          << " != 1 (diff=" << tTotal - 1.0 << ")\n";
+            }
 
         }
+
+        std::cout << "Flux: " << flux << '\n';
 
         // cleanup
         mnt_grid_del(&srcGrid);
