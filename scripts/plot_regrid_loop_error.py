e60e722f32f85897d233f5d6eb618b3c15d7884b
Alex Pletzer <alexander@gokliya.net>
2018-07-21 15:46:06 +1200

    new values

--- scripts/plot_regrid_loop_error.py
+++ scripts/plot_regrid_loop_error.py
@@ -1,12 +1,14 @@
 from matplotlib import pylab
 from math import sqrt
 
-ncells = [4050, 16200, 64800, 259200, 1036800, 4147200, 16588800, 66355200  ]
-loopVertsErrors = [0.43250632328, 0.21322909097, 0.109496002498, 0.0546506820885, 0.0258448920252, 0.0137091999867, 0.00668680902321,  0.00435628670771]
-loopEdgesErrors = [6.33174e-16, 3.88578e-16, 1.97238e-15, 1.98279e-15, 1.49378e-13, 1.3365e-13, 1.28068e-13,  2.70251e-13]
+ncells = [4050, 16200, 64800, 259200, 1036800, 4147200, 16588800, 66355200 ]
+
+loopVertsErrors = [0.432506323032, 0.213229090732, 0.109496002478, 0.0546506820968, 0.0258635444994, 0.0137091999851, 0.00668680899773, 0.00435628670767 ]
+loopEdgesErrors = [5.25621e-16, 7.26849e-16, 2.41231e-14, 2.35502e-14, 2.32577e-14, 3.83547e-14, 3.83174e-14,  float('nan')]
+
 pylab.loglog(ncells, loopVertsErrors, 'c--')
 pylab.loglog(ncells, loopEdgesErrors, 'm-')
-pylab.legend(['bilinear', 'mimetic'])
+pylab.legend(['bilinear', 'mimetic'], loc='center right')
 pylab.loglog(ncells, loopVertsErrors, 'ko', markerfacecolor='None')
 pylab.loglog(ncells, loopEdgesErrors, 'ks', markerfacecolor='None')
 pylab.loglog([4000, 1e8], [1./sqrt(4000), 1./sqrt(1e8)], 'k-.')
