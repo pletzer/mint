fb81cc28f89d03587d09a7de8759db37ff4180f1
Alex Pletzer <alexander@gokliya.net>
2018-08-26 11:23:37 +1200

    + error messages

--- tests/testCellLocatorFromFileF.f90
+++ tests/testCellLocatorFromFileF.f90
@@ -24,16 +24,26 @@ program test
 
 
     ier = mnt_cmdlineargparser_new(prsr)
+    if (ier /= 0) write(0, *) 'ERROR after mnt_cmdlineargparser_new'
+
     ier = mnt_cmdlineargparser_setint(prsr, "-n"//char(0), 100, &
                                          "Average number of cells per bucket"//char(0))
+    if (ier /= 0) write(0, *) 'ERROR after mnt_cmdlineargparser_new'
+
     ier = mnt_cmdlineargparser_setstring(prsr, "-i"//char(0), &
                                          "notvalid.vtk"//char(0), &
                                          "Input VTK file"//char(0))
+    if (ier /= 0) write(0, *) 'ERROR after mnt_cmdlineargparser_setstring -i'
+
     ier = mnt_cmdlineargparser_setstring(prsr, "-o"//char(0), &
                                          "testCellLocatorFromFile_grid.vtk"//char(0), &
                                          "Output VTK file"//char(0))
+    if (ier /= 0) write(0, *) 'ERROR after mnt_cmdlineargparser_setstring -o'
+
     ier = mnt_cmdlineargparser_setbool(prsr, "-v"//char(0), 0, &
                                          "Turn verbosity on"//char(0))
+    if (ier /= 0) write(0, *) 'ERROR after mnt_cmdlineargparser_setbool'
+
     ! parse
     nargs = command_argument_count() + 1 ! must include the executable's name
     allocate(args(nargs))
@@ -43,71 +53,81 @@ program test
         ! add termination character, trim...
         call mnt_f2c_string(argv_full, args(i))
     enddo
-    ier = mnt_cmdlineargparser_help(prsr)
 
     ier = mnt_cmdlineargparser_parse(prsr, nargs, mnt_string_size, args)
+    if (ier /= 0) write(0, *) 'ERROR after mnt_cmdlineargparser_parse'
 
     ier = mnt_cmdlineargparser_help(prsr)
+    if (ier /= 0) write(0, *) 'ERROR after mnt_cmdlineargparser_help'
 
     ! extract the arguments
     ier = mnt_cmdlineargparser_getint(prsr, "-n"//char(0), num_cells_per_bucket)
+    if (ier /= 0) write(0, *) 'ERROR after mnt_cmdlineargparser_getint'
+
     ier = mnt_cmdlineargparser_getstring(prsr, "-i"//char(0), inp_filename, n)
+    if (ier /= 0) write(0, *) 'ERROR after mnt_cmdlineargparser_getstring -i'
     call mnt_c2f_string(inp_filename, inp_filename_f)
+
     ier = mnt_cmdlineargparser_getstring(prsr, "-o"//char(0), out_filename, n)
+    if (ier /= 0) write(0, *) 'ERROR after mnt_cmdlineargparser_getstring -o'
     call mnt_c2f_string(out_filename, out_filename_f)
+
     ier = mnt_cmdlineargparser_getbool(prsr, "-v"//char(0), verbosity)
+    if (ier /= 0) write(0, *) 'ERROR after mnt_cmdlineargparser_getbool'
 
     ! done
     ier = mnt_cmdlineargparser_del(prsr)
+    if (ier /= 0) write(0, *) 'ERROR after mnt_cmdlineargparser_del'
 
     if (inp_filename == 'not-valid.vtk') then
         stop 'ERROR: must provide VTK file name (-i <filename>)'
     endif
 
-    print *,'Reading grid from file "'//trim(inp_filename_f)//'"'
-    print *,'Number of cells per bucket = ', num_cells_per_bucket
-    print *,'Output file name = "'//trim(out_filename_f)//'"'
+    write(0, *) 'Reading grid from file "'//trim(inp_filename_f)//'"'
+    write(0, *) 'Number of cells per bucket = ', num_cells_per_bucket
+    write(0, *) 'Output file name = "'//trim(out_filename_f)//'"'
 
     ier = mnt_celllocator_new(cloc)
-    if(ier /= 0) print *,'ERROR after new ier = ', ier
+    if(ier /= 0) write(0, *) 'ERROR after mnt_celllocator_new ier = ', ier
 
     ier = mnt_celllocator_load(cloc, inp_filename, len(trim(inp_filename), c_size_t))
     if(ier /= 0) then 
-        stop 'ERROR after load'
+        write(0, *) 'ERROR after mnt_celllocator_load ier = ', ier
+        stop
     endif
 
     ier = mnt_celllocator_build(cloc, num_cells_per_bucket)
-    if(ier /= 0) print *,'ERROR ier = after build', ier
+    if(ier /= 0) write(0, *) 'ERROR after mnt_celllocator_build ier = ', ier
 
     if (verbosity /= 0) then
         ier = mnt_celllocator_rungriddiagnostics(cloc)
-        if(ier /= 0) print *,'ERROR ier = after rungriddiagnostics', ier
+        if(ier /= 0) write(0, *) 'ERROR ier = after mnt_celllocator_rungriddiagnostics ier = ', ier
     endif
 
     ! initialiaze
     pcoords = -1._c_double
     ier = mnt_celllocator_find(cloc, target_point(1), cell_id, pcoords(1))
-    if(ier /= 0) print *,'ERROR ier = after find', ier
+    if(ier /= 0) write(0, *) 'ERROR ier = after mnt_celllocator_find ier = ', ier
 
     if (cell_id >= 0) then
         ! the target point was found
 
         ier = mnt_celllocator_interp_point(cloc, cell_id, pcoords(1), interp_point(1))
-        if(ier /= 0) print *,'ERROR ier = after find', ier
+        if(ier /= 0) write(0, *) 'ERROR ier = after mnt_celllocator_interp_point ier = ', ier
 
         ! check interpolation
         diff2 = dot_product(interp_point - target_point, interp_point - target_point)
-        print *,'distance square error = ', diff2
+        write(0, *) 'distance square error = ', diff2
     endif
 
     ier = mnt_celllocator_dumpgrid(cloc, trim(out_filename), len(trim(out_filename), c_size_t))
-    if(ier /= 0) print *,'ERROR after dumpgrid ier = ', ier
+    if(ier /= 0) write(0, *) 'ERROR after mnt_celllocator_dumpgrid ier = ', ier
 
     ! clean up
     deallocate(args)
     ier = mnt_celllocator_del(cloc)
-    if(ier /= 0) print *,'ERROR after del ier = ', ier
+    if(ier /= 0) write(0, *) 'ERROR after del ier = ', ier
 
-    print *,'cell_id: ', cell_id, ' pcoords = ', pcoords
+    write(0, *) 'cell_id: ', cell_id, ' pcoords = ', pcoords
 
 end program 
