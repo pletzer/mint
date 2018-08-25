569d52d1bd8d7be98968566c9649a6ca9aef61ec
Alex Pletzer <alexander@gokliya.net>
2018-08-25 23:47:54 +0000

    test now uses allocatable array

--- tests/testCellLocatorF.f90
+++ tests/testCellLocatorF.f90
@@ -10,7 +10,7 @@ program test
     integer(c_int), parameter    :: num_verts_per_cell = 8
     integer(c_size_t), parameter :: num_points = num_cells*num_verts_per_cell
     real(c_double)               :: verts(num_points * 3)
-    real(c_double)               :: target_point(3) = [0.2_c_double, 0.3_c_double, 0.4_c_double]
+    real(c_double), allocatable  :: target_point(:) 
     real(c_double)               :: interp_point(3)
     real(c_double)               :: pcoords(3)
     real(c_double)       :: diff2
@@ -36,6 +36,8 @@ program test
     ier = mnt_celllocator_build(cloc, num_cells_per_bucket)
     if(ier /= 0) print*,'ERROR ier = after build', ier
 
+    allocate(target_point(3))
+    target_point = [0.2_c_double, 0.3_c_double, 0.4_c_double]
     ier = mnt_celllocator_find(cloc, target_point(1), cell_id, pcoords(1))
     if(ier /= 0) print*,'ERROR ier = after find', ier
 
@@ -44,6 +46,7 @@ program test
     ! check interpolation
     diff2 = dot_product(interp_point - target_point, interp_point - target_point)
     print *,'distance square error = ', diff2
+    deallocate(target_point)
 
     ier = mnt_celllocator_del(cloc)
     if(ier /= 0) print*,'ERROR after del ier = ', ier
