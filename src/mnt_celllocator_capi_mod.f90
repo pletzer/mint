cc11752522ea28ca6a239d8eea8c07dad38dabf6
Alex Pletzer <alexander@gokliya.net>
2018-08-26 11:06:33 +1200

    + comments and fixed build interface

--- src/mnt_celllocator_capi_mod.f90
+++ src/mnt_celllocator_capi_mod.f90
@@ -3,6 +3,9 @@ module mnt_celllocator_capi_mod
   interface
 
     function mnt_celllocator_new(obj) bind(C)
+      ! Constructor
+      ! @param obj instance of mntcellLocator_t (opaque handle)
+      ! @return 0 if successful
       use, intrinsic :: iso_c_binding, only: c_size_t, c_int, c_double
       implicit none
       integer(c_size_t), intent(inout) :: obj
@@ -10,6 +13,9 @@ module mnt_celllocator_capi_mod
     end function mnt_celllocator_new
 
     function mnt_celllocator_del(obj) bind(C)
+      ! Destructor
+      ! @param obj instance of mntcellLocator_t (opaque handle)
+      ! @return 0 if successful
       use, intrinsic :: iso_c_binding, only: c_size_t, c_int, c_double
       implicit none
       integer(c_size_t), intent(inout) :: obj
@@ -17,6 +23,10 @@ module mnt_celllocator_capi_mod
     end function mnt_celllocator_del
 
     function mnt_celllocator_load(obj, filename, n) bind(C)
+      ! Load cell locator object from file 
+      ! @param obj instance of mntcellLocator_t (opaque handle)
+      ! @return 0 if sucecssful
+      ! @note must invoke constructior prior to this call
       use, intrinsic :: iso_c_binding, only: c_size_t, c_int, c_double
       implicit none
       integer(c_size_t), intent(inout)         :: obj
@@ -26,6 +36,12 @@ module mnt_celllocator_capi_mod
     end function mnt_celllocator_load
 
     function mnt_celllocator_setpoints(obj, nverts_per_cell, ncells, verts) bind(C)
+      ! Set the grid points
+      ! @param obj instance of mntcellLocator_t (opaque handle)
+      ! @param nverts_per_cell number of vertices per cell
+      ! @param ncells number of cells
+      ! @param verts flat array of vertices [x0, y0, z0, x1, y1, z1, ...]
+      ! @return 0 if successful
       use, intrinsic :: iso_c_binding, only: c_size_t, c_int, c_double
       implicit none
       integer(c_size_t), intent(inout)         :: obj
@@ -36,14 +52,24 @@ module mnt_celllocator_capi_mod
     end function mnt_celllocator_setpoints
 
     function mnt_celllocator_build(obj, num_cells_per_bucket) bind(C)
+      ! Build locator object
+      ! @param obj instance of mntcellLocator_t (opaque handle)
+      ! @param um_cells_per_bucket number of cells per bucket
+      ! @return 0 if successful
       use, intrinsic :: iso_c_binding, only: c_size_t, c_int, c_double
       implicit none
       integer(c_size_t), intent(inout) :: obj
-      integer(c_int), intent(in)       :: num_cells_per_bucket
+      integer(c_int), value            :: num_cells_per_bucket
       integer(c_int)                   :: mnt_celllocator_build
     end function mnt_celllocator_build
 
     function mnt_celllocator_find(obj, point, cell_id, pcoords) bind(C)
+      ! Find point
+      ! @param obj instance of mntcellLocator_t (opaque handle)
+      ! @param point 3d point
+      ! @param cell_id output cell Id (zero-based)
+      ! @param output parametric coordinates
+      ! @return 0 if successful
       use, intrinsic :: iso_c_binding, only: c_size_t, c_int, c_double
       implicit none
       integer(c_size_t), intent(inout)         :: obj
@@ -54,6 +80,13 @@ module mnt_celllocator_capi_mod
     end function mnt_celllocator_find
 
     function mnt_celllocator_interp_point(obj, cell_id, pcoords, point) bind(C)
+      ! Interpolate point
+      ! @param obj instance of mntcellLocator_t (opaque handle)
+      ! @param cell_id 0-based cell Id
+      ! @param pcoords array of parametric coordinates
+      ! @param point output point
+      ! @return 0 if successful
+      ! @note call mnt_celllocator_find to compute pcoords and get cell_id
       use, intrinsic :: iso_c_binding, only: c_size_t, c_int, c_double
       implicit none
       integer(c_size_t), intent(inout)         :: obj
@@ -64,6 +97,11 @@ module mnt_celllocator_capi_mod
     end function mnt_celllocator_interp_point
 
     function mnt_celllocator_dumpgrid(obj, filename, n) bind(C)
+      ! Dump grid to VTK file
+      ! @param obj instance of mntcellLocator_t (opaque handle)
+      ! @param filename file name, \0 (char(0)) terminated
+      ! @param n length of filename 
+      ! @return 0 if successful
       use, intrinsic :: iso_c_binding, only: c_size_t, c_int, c_double
       implicit none
       integer(c_size_t), intent(inout)         :: obj
@@ -73,6 +111,9 @@ module mnt_celllocator_capi_mod
     end function mnt_celllocator_dumpgrid
 
     function mnt_celllocator_rungriddiagnostics(obj) bind(C)
+      ! Run grid diagnostics
+      ! @param obj instance of mntcellLocator_t (opaque handle)
+      ! @return 0 if successful
       use, intrinsic :: iso_c_binding, only: c_size_t, c_int, c_double
       implicit none
       integer(c_size_t), intent(inout)         :: obj
