e1b939bb792cfc5444504e059107c54685940a3a
Alex Pletzer <alexander@gokliya.net>
2018-08-25 15:56:59 +1200

    ~ changed interface by requiring pointers to be passed

--- src/mnt_celllocator_capi_mod.f90
+++ src/mnt_celllocator_capi_mod.f90
@@ -23,11 +23,11 @@ module mnt_celllocator_capi_mod
     end function mnt_celllocator_load
 
     function mnt_celllocator_setpoints(obj, nverts_per_cell, ncells, verts) bind(C)
-      use, intrinsic :: iso_c_binding, only: c_size_t, c_int, c_double
+      use, intrinsic :: iso_c_binding, only: c_size_t, c_int, c_double, c_ptr
       integer(c_size_t), intent(inout)         :: obj
       integer(c_int), value                    :: nverts_per_cell
       integer(c_size_t), value                 :: ncells
-      real(c_double), intent(in)               :: verts(*) ! const double*
+      type(c_ptr), intent(in)                  :: verts ! const double*
       integer(c_int)                           :: mnt_celllocator_setpoints
     end function mnt_celllocator_setpoints
 
@@ -39,20 +39,20 @@ module mnt_celllocator_capi_mod
     end function mnt_celllocator_build
 
     function mnt_celllocator_find(obj, point, cell_id, pcoords) bind(C)
-      use, intrinsic :: iso_c_binding, only: c_size_t, c_int, c_double
+      use, intrinsic :: iso_c_binding, only: c_size_t, c_int, c_double, c_ptr
       integer(c_size_t), intent(inout)         :: obj
-      real(c_double), intent(in)               :: point(*)   ! const double*
+      type(c_ptr), intent(in)                  :: point   ! const double*
       integer(c_size_t), intent(out)           :: cell_id
-      real(c_double), intent(out)              :: pcoords(*) ! double*
+      type(c_ptr), intent(out)                 :: pcoords ! double*
       integer(c_int)                           :: mnt_celllocator_find
     end function mnt_celllocator_find
 
     function mnt_celllocator_interp_point(obj, cell_id, pcoords, point) bind(C)
-      use, intrinsic :: iso_c_binding, only: c_size_t, c_int, c_double
+      use, intrinsic :: iso_c_binding, only: c_size_t, c_int, c_double, c_ptr
       integer(c_size_t), intent(inout)         :: obj
       integer(c_size_t), value                 :: cell_id
-      real(c_double), intent(in)               :: pcoords(*) ! const double*
-      real(c_double), intent(out)              :: point(*)   ! double*
+      type(c_ptr), intent(in)                  :: pcoords ! const double*
+      type(c_ptr), intent(out)                 :: point   ! double*
       integer(c_int)                           :: mnt_celllocator_find
     end function mnt_celllocator_interp_point
 
--- tests/testCellLocatorF.f90
+++ tests/testCellLocatorF.f90
@@ -2,20 +2,21 @@ program test
     ! defines the C API
     use mnt_celllocator_capi_mod
 
-    use, intrinsic :: iso_c_binding, only: c_size_t, c_int, c_double
+    use, intrinsic :: iso_c_binding, only: c_size_t, c_int, c_double, c_ptr, c_loc
 
     implicit none
     integer(c_size_t) :: cloc
     integer(c_size_t), parameter :: num_cells = 1
     integer(c_int), parameter    :: num_verts_per_cell = 8
     integer(c_size_t), parameter :: num_points = num_cells*num_verts_per_cell
-    real(c_double)       :: verts(num_points * 3)
-    real(c_double)       :: target_point(3) = [0.2_c_double, 0.3_c_double, 0.4_c_double]
-    real(c_double)       :: interp_point(3)
-    real(c_double)       :: pcoords(3)
+    real(c_double), target       :: verts(num_points * 3)
+    real(c_double), target       :: target_point(3) = [0.2_c_double, 0.3_c_double, 0.4_c_double]
+    real(c_double), target       :: interp_point(3)
+    real(c_double), target       :: pcoords(3)
     real(c_double)       :: diff2
-    integer(c_int)    :: ier, num_cells_per_bucket
-    integer(c_size_t) :: cell_id
+    integer(c_int)       :: ier, num_cells_per_bucket
+    integer(c_size_t)    :: cell_id
+    type(c_ptr)          :: pcoords_ptr, interp_point_ptr
 
     verts = [0., 0., 0., &
              1., 0., 0., &
@@ -28,15 +29,20 @@ program test
 
     ier = mnt_celllocator_new(cloc)
     if(ier /= 0) print*,'ERROR after new ier = ', ier
-    ier = mnt_celllocator_setpoints(cloc, num_verts_per_cell, num_cells, verts(1))
+
+    ier = mnt_celllocator_setpoints(cloc, num_verts_per_cell, num_cells, c_loc(verts(1)))
     if(ier /= 0) print*,'ERROR after setpoints ier = ', ier
+
     num_cells_per_bucket = 512
     ier = mnt_celllocator_build(cloc, num_cells_per_bucket)
     if(ier /= 0) print*,'ERROR ier = after build', ier
-    ier = mnt_celllocator_find(cloc, target_point(1), cell_id, pcoords(1))
+
+    pcoords_ptr = c_loc(pcoords(1))
+    ier = mnt_celllocator_find(cloc, c_loc(target_point(1)), cell_id, pcoords_ptr)
     if(ier /= 0) print*,'ERROR ier = after find', ier
 
-    ier = mnt_celllocator_interp_point(cloc, cell_id, pcoords(1), interp_point(1))
+    interp_point_ptr = c_loc(interp_point(1))
+    ier = mnt_celllocator_interp_point(cloc, cell_id, c_loc(pcoords(1)), interp_point_ptr)
     if(ier /= 0) print*,'ERROR ier = after find', ier
     ! check interpolation
     diff2 = dot_product(interp_point - target_point, interp_point - target_point)
