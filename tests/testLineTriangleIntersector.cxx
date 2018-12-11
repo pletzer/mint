f7147e8eda511c16961f0e33672b541ccc8b9774
Alex Pletzer <alexander@gokliya.net>
2018-12-12 11:58:09 +1300

    + degenerate test, fails

--- tests/testLineTriangleIntersector.cxx
+++ tests/testLineTriangleIntersector.cxx
@@ -16,18 +16,18 @@ void testNormal() {
     double q0[] = {0., 0., 0.};
     double q1[] = {1., 0., 0.};
     double q2[] = {0., 1., 0.};
-    LineTriangleIntersector lli;
-    lli.setPoints(p0, p1, q0, q1, q2);
+    LineTriangleIntersector lti;
+    lti.setPoints(p0, p1, q0, q1, q2);
 
-    double det = lli.getDet();
-    assert(abs(det - (-1.)) < TOL);
-
-    Vector<double> sol = lli.getSolution();
+    double det = lti.getDet();
+    assert(std::abs(det - (-1.)) < TOL);
+    assert(lti.hasSolution(TOL));
+    Vector<double> sol = lti.getSolution();
     double lam = sol[0];
-    assert(abs(lam - 1.0) < TOL);
+    assert(std::abs(lam - 1.0) < TOL);
     std::vector<double> xi = std::vector<double>(&sol[1], &sol[3]);
-    assert(abs(xi[0] - 0.) < TOL);
-    assert(abs(xi[1] - 0.) < TOL);
+    assert(std::abs(xi[0] - 0.) < TOL);
+    assert(std::abs(xi[1] - 0.) < TOL);
 }
 
 void testNoSolution() {
@@ -40,20 +40,38 @@ void testNoSolution() {
     double q0[] = {0., 0., 0.};
     double q1[] = {1., 0., 0.};
     double q2[] = {0., 1., 0.};
-    LineTriangleIntersector lli;
-    lli.setPoints(p0, p1, q0, q1, q2);
+    LineTriangleIntersector lti;
+    lti.setPoints(p0, p1, q0, q1, q2);
 
-    double det = lli.getDet();
-    assert(abs(det - (0.)) < TOL);
-    assert(!lli.hasSolution(TOL));
+    double det = lti.getDet();
+    assert(std::abs(det - (0.)) < TOL);
+    assert(!lti.hasSolution(TOL));
 }
 
+void testDegenerate() {
+
+    // line
+    double p0[] = {0., 0., 0.};
+    double p1[] = {0., 1., 0.};
+
+    // triangle
+    double q0[] = {0., 0., 0.};
+    double q1[] = {1., 0., 0.};
+    double q2[] = {0., 1., 0.};
+    LineTriangleIntersector lti;
+    lti.setPoints(p0, p1, q0, q1, q2);
+
+    double det = lti.getDet();
+    assert(std::abs(det - (0.)) < TOL);
+    assert(lti.hasSolution(TOL));
+}
 
 
 int main(int argc, char** argv) {
 
     testNormal();
     testNoSolution();
+    testDegenerate();
 
     return 0;
 }
