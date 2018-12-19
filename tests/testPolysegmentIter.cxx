38fa95c28a1ce52b11c8982845a9d2f75ca4e976
Alex Pletzer <alexander@gokliya.net>
2018-12-20 10:56:52 +1300

    + test where the line is completely outside the grid

--- tests/testPolysegmentIter.cxx
+++ tests/testPolysegmentIter.cxx
@@ -64,6 +64,61 @@ void test1Cell() {
     points->Delete();
 }
 
+void test1CellLineOutside() {
+    // a one cell grid, built from scratch
+    double v0[] = {0., 0., 0.};
+    double v1[] = {1., 0., 0.};
+    double v2[] = {1., 1., 0.};
+    double v3[] = {0., 1., 0.};
+    vtkPoints* points = vtkPoints::New();
+    points->SetDataTypeToDouble();
+    points->InsertNextPoint(v0);
+    points->InsertNextPoint(v1);
+    points->InsertNextPoint(v2);
+    points->InsertNextPoint(v3);
+
+    vtkUnstructuredGrid* grid = vtkUnstructuredGrid::New();
+    grid->SetPoints(points);
+    grid->Allocate(1, 1);
+    vtkIdList* ptIds = vtkIdList::New();
+    ptIds->SetNumberOfIds(4);
+    for (vtkIdType i = 0; i < 4; ++i) {
+        ptIds->SetId(i, i);
+    }
+    grid->InsertNextCell(VTK_QUAD, ptIds);
+    
+
+    vtkCellLocator* loc = vtkCellLocator::New();
+    loc->SetDataSet(grid);
+    loc->BuildLocator();
+
+    const double p0[] = {0.2, 0.3, -1.};
+    const double p1[] = {0.4, 0.5, -1.};
+    PolysegmentIter psi(grid, loc, p0, p1);
+    size_t numSegs = psi.getNumberOfSegments();
+    psi.reset();
+    for (size_t i = 0; i < numSegs; ++i) {
+        vtkIdType cellId = psi.getCellId();
+        const std::vector<double>& xia = psi.getBegCellParamCoord();
+        const std::vector<double>& xib = psi.getEndCellParamCoord();
+        double ta = psi.getBegLineParamCoord();
+        double tb = psi.getEndLineParamCoord();
+        double coeff = psi.getCoefficient();
+        std::cout << "test1CellLineOutside: seg " << i << " cell=" << cellId \
+                                   << " ta=" << ta << " xia=" << xia[0] << ',' << xia[1] 
+                                   << " tb=" << tb << " xib=" << xib[0] << ',' << xib[1] 
+                                   << '\n';
+        psi.next();
+    }
+    assert(std::abs(psi.getIntegratedParamCoord() - 0.0) < 1.e-10);
+
+    ptIds->Delete();
+    loc->Delete();
+    grid->Delete();
+    points->Delete();
+}
+
+
 void test2Cells() {
     // two cell grid, built from scratch
     double v0[] = {0., 0., 0.};
@@ -120,7 +175,7 @@ void test2Cells() {
                                    << '\n';
         psi.next();
     }
-    assert(std::abs(psi.getIntegratedParamCoord() - 1.0) < 1.e-10);
+    assert(std::abs(psi.getIntegratedParamCoord() - 0.0) < 1.e-10);
 
     ptIds->Delete();
     loc->Delete();
@@ -198,6 +253,7 @@ void test2CellsEdge() {
 
 int main(int argc, char** argv) {
 
+	test1CellLineOutside();
     test1Cell();
     test2Cells();
     test2CellsEdge();
