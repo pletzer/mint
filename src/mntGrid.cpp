1f7efb6769b4380f8b256dcfe77e10f6e0e81711
Alex Pletzer <alexander@gokliya.net>
2019-01-29 16:07:17 +1300

    code cleanup

--- src/mntGrid.cpp
+++ src/mntGrid.cpp
@@ -187,12 +187,8 @@ int mnt_grid_loadFrom2DUgrid(Grid_t** self, const char* filename) {
         int ier2 = nc_get_att_text(ncid, ivar, "long_name", &long_name[0]);
         int ier3 = nc_get_att_text(ncid, ivar, "cf_role", &cf_role[0]);
 
-        std::cerr << "*** varname = " << varname << '\n';
-        std::cerr << "*** standard_name = " << standard_name << " long_name = " << long_name << " cf_role = " << cf_role << '\n';
-        std::cerr << "*** ier1 = " << ier1 << " ier2 = " << ier2 << " ier3 = " << ier3 << '\n';
-
         if (ier1 == NC_NOERR && ier2 == NC_NOERR) {
-            std::cerr << "... var has standard_name and long_name\n";
+
             // variable has "standard_name" and long_name attributes
 
             // get the number of elements
@@ -206,7 +202,6 @@ int mnt_grid_loadFrom2DUgrid(Grid_t** self, const char* filename) {
                 }
                     nelems *= dim;
             }
-            std::cerr << "*** ndims = " << ndims << " nelems = " << nelems << '\n';
 
             // is it a latitude or a longitude?
             if (standard_name.find("latitude") != std::string::npos &&
@@ -217,20 +212,18 @@ int mnt_grid_loadFrom2DUgrid(Grid_t** self, const char* filename) {
 
                 if (xtype == NC_DOUBLE) {
                     // read the latitudes as doubles
-                    std::cerr << "==== reading lats as doubles\n";
                     ier = nc_get_var_double(ncid, ivar, &lats[0]);
                     if (ier != NC_NOERR) {
-                        std::cerr << "ERROR: after reading cell latitudes as doubles (ier = " << ier << ")\n";
+                        std::cerr << "ERROR: after reading latitudes as doubles (ier = " << ier << ")\n";
                         return 1;
                     }
                 }
                 else if (xtype == NC_FLOAT) {
                     // read the latitudes as floats
-                    std::cerr << "==== reading lats as floats\n";
                     std::vector<float> data(nelems);
                     ier = nc_get_var_float(ncid, ivar, &data[0]);
                     if (ier != NC_NOERR) {
-                        std::cerr << "ERROR: after reading cell latitudes as floats (ier = " << ier << ")\n";
+                        std::cerr << "ERROR: after reading latitudes as floats (ier = " << ier << ")\n";
                         return 1;
                     }
                     // copy 
@@ -246,23 +239,26 @@ int mnt_grid_loadFrom2DUgrid(Grid_t** self, const char* filename) {
             else if (standard_name.find("longitude") != std::string::npos && 
                      long_name.find("node") != std::string::npos) {
 
-                // allocate the data to receive the lons
+                // allocate to receive the lons
                 lons.resize(nelems);
 
                 if (xtype == NC_DOUBLE) {
                     // read the longitudes as doubles
-                    std::cerr << "==== reading lons as doubles\n";
                     ier = nc_get_var_double(ncid, ivar, &lons[0]);
                     if (ier != NC_NOERR) {
-                        std::cerr << "ERROR: after reading cell longitudes (ier = " << ier << ")\n";
+                        std::cerr << "ERROR: after reading longitudes as double (ier = " << ier << ")\n";
                         return 1;
                     }
                 }
                 else if (xtype == NC_FLOAT) {
                     // read the longitudes as floats
-                    std::cerr << "==== reading lons as floats\n";
                     std::vector<float> data(nelems);
                     ier = nc_get_var_float(ncid, ivar, &data[0]);
+                    if (ier != NC_NOERR) {
+                        std::cerr << "ERROR: after reading longitudes as floats (ier = " << ier << ")\n";
+                        return 1;
+                    }
+                    // copy into double array
                     for (size_t i = 0; i < nelems; ++i) {
                         lons[i] = (double) data[i];
                     }
@@ -282,7 +278,6 @@ int mnt_grid_loadFrom2DUgrid(Grid_t** self, const char* filename) {
                 quad_connectivity.resize(nelems);
 
                 // read the connectivity
-                std::cerr << "==== reading quad_connectivity\n";
                 ier = nc_get_var_int(ncid, ivar, &quad_connectivity[0]);
                 if (ier != NC_NOERR) {
                     std::cerr << "ERROR: after reading cell connectivity (ier = " << ier << ")\n";
@@ -305,12 +300,10 @@ int mnt_grid_loadFrom2DUgrid(Grid_t** self, const char* filename) {
 
         for (int icell = 0; icell < ncells; ++icell) {
 
-            std::cerr << "*** cell " << icell << '\n';
             for (int node = 0; node < four; ++node) {
 
                 int k = quad_connectivity[node + four*icell];
 
-                std::cerr << "\tk = " << k << " lat = " << lats[k] << " lon = " << lons[k] << '\n';
                 // even in 2d we have three components
                 (*self)->verts[0 + node*3 + icell*four*3] = lats[k];
                 (*self)->verts[1 + node*3 + icell*four*3] = lons[k];
