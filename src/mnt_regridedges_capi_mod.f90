08c23dde3b3388d013f4aff59d0902482224727b
Alex Pletzer <alexander@gokliya.net>
2019-01-31 16:28:35 +1300

    ~ working on fortran callable regrid API

--- src/mnt_regridedges_capi_mod.f90
+++ src/mnt_regridedges_capi_mod.f90
@@ -5,7 +5,7 @@ module mnt_regridedges_capi_mod
     function mnt_regridedges_new(obj) &
                               &  bind(C, name='mnt_regridedges_new')
       ! Constructor
-      ! @param obj opaque handle
+      ! @param obj instance of mntRegridEdges_t (opaque handle)
       ! @return 0 if successful
       use, intrinsic :: iso_c_binding, only: c_int, c_double, c_ptr
       implicit none
@@ -16,7 +16,7 @@ module mnt_regridedges_capi_mod
     function mnt_regridedges_del(obj) &
                                & bind(C, name='mnt_regridedges_del')
       ! Destructor
-      ! @param obj opaque handle
+      ! @param obj instance of mntRegridEdges_t (opaque handle)
       ! @return 0 if successful
       use, intrinsic :: iso_c_binding, only: c_int, c_double, c_ptr
       implicit none
@@ -24,59 +24,42 @@ module mnt_regridedges_capi_mod
       integer(c_int)                   :: mnt_regridedges_del
     end function mnt_regridedges_del
 
-    function mnt_regridedges_load(obj, filename, n) &
-                                & bind(C, name='mnt_regridedges_load')
-      ! Load weights from file 
-      ! @param obj opaque handle
+    function mnt_regridedges_loadsrc(obj, filename, n) &
+                                & bind(C, name='mnt_regridedges_loadSrc')
+      ! Load source grid from 2d UGRID file 
+      ! @param obj instance of mntRegridEdges_t (opaque handle)
       ! @param filename file name
+      ! @param n length of string filename
       ! @return 0 if sucecssful
       ! @note must invoke constructor prior to this call
-      use, intrinsic :: iso_c_binding, only: c_size_t, c_int, c_double, c_ptr, c_char
+      use, intrinsic :: iso_c_binding, only: c_size_t, c_int, c_ptr, c_char
       implicit none
       type(c_ptr), intent(inout)               :: obj ! void**
       character(kind=c_char), intent(in)       :: filename(*)
       integer(c_size_t), value                 :: n
-      integer(c_int)                           :: mnt_regridedges_load
-    end function mnt_regridedges_load
+      integer(c_int)                           :: mnt_regridedges_loadsrc
+    end function mnt_regridedges_loadsrc
 
-    function mnt_regridedges_setSrcPointsPtr(obj, nverts_per_cell, ncells, verts) &
-                                          &  bind(C, name='mnt_regridedges_setSrcPointsPtr')
-      ! Set the source grid points
-      ! @param obj opaque handle
-      ! @param nverts_per_cell number of vertices per cell
-      ! @param ncells number of cells
-      ! @param verts flat array of vertices [x0, y0, z0, x1, y1, z1, ...]
-      ! @return 0 if successful
-      use, intrinsic :: iso_c_binding, only: c_size_t, c_int, c_double, c_ptr
+    function mnt_regridedges_loaddst(obj, filename, n) &
+                                & bind(C, name='mnt_regridedges_loadDst')
+      ! Load destination grid from 2d UGRID file 
+      ! @param obj instance of mntRegridEdges_t (opaque handle)
+      ! @param filename file name
+      ! @param n length of string filename
+      ! @return 0 if sucecssful
+      ! @note must invoke constructor prior to this call
+      use, intrinsic :: iso_c_binding, only: c_size_t, c_int, c_ptr, c_char
       implicit none
       type(c_ptr), intent(inout)               :: obj ! void**
-      integer(c_int), value                    :: nverts_per_cell
-      integer(c_size_t), value                 :: ncells
-      real(c_double), intent(in)               :: verts(*) ! const double*
-      integer(c_int)                           :: mnt_regridedges_setsrcpointsptr
-    end function mnt_regridedges_setSrcPointsPtr
-
-    function mnt_regridedges_setDstPointsPtr(obj, nverts_per_cell, ncells, verts) &
-                                          &  bind(C, name='mnt_regridedges_setDstPointsPtr')
-      ! Set the destination grid points
-      ! @param obj opaque handle
-      ! @param nverts_per_cell number of vertices per cell
-      ! @param ncells number of cells
-      ! @param verts flat array of vertices [x0, y0, z0, x1, y1, z1, ...]
-      ! @return 0 if successful
-      use, intrinsic :: iso_c_binding, only: c_size_t, c_int, c_double, c_ptr
-      implicit none
-      type(c_ptr), intent(inout)               :: obj ! void**
-      integer(c_int), value                    :: nverts_per_cell
-      integer(c_size_t), value                 :: ncells
-      real(c_double), intent(in)               :: verts(*) ! const double*
-      integer(c_int)                           :: mnt_regridedges_setdstpointsptr
-    end function mnt_regridedges_setDstPointsPtr
+      character(kind=c_char), intent(in)       :: filename(*)
+      integer(c_size_t), value                 :: n
+      integer(c_int)                           :: mnt_regridedges_loaddst
+    end function mnt_regridedges_loaddst
 
     function mnt_regridedges_build(obj, num_cells_per_bucket) &
                                  & bind(C, name='mnt_regridedges_build')
-      ! Build the regridder
-      ! @param obj opaque handle
+      ! Build locator object
+      ! @param obj instance of mntregridedges_t (opaque handle)
       ! @param num_cells_per_bucket number of cells per bucket
       ! @return 0 if successful
       use, intrinsic :: iso_c_binding, only: c_int, c_double, c_ptr
@@ -86,34 +69,19 @@ module mnt_regridedges_capi_mod
       integer(c_int)                   :: mnt_regridedges_build
     end function mnt_regridedges_build
 
-    function mnt_regridedges_applyWeights(obj, src_data, dst_data) &
-                                & bind(C, name='mnt_regridedges_applyWeights')
-      ! Apply the interpolation weights
-      ! @param obj opaque handle
-      ! @param src_data edge centred data on the source grid 
-      ! @param dst_data edge centred data on the destination grid 
-      ! @return 0 if successful
-      use, intrinsic :: iso_c_binding, only: c_long_long, c_int, c_double, c_ptr
-      implicit none
-      type(c_ptr), intent(inout)               :: obj ! void**
-      real(c_double), intent(in)               :: src_data(*) ! const double*
-      real(c_double), intent(out)              :: dst_data(*) ! double*
-      integer(c_int)                           :: mnt_regridedges_applyWeights
-    end function mnt_regridedges_applyWeights
-
     function mnt_regridedges_dump(obj, filename, n) & 
-                                & bind(C, name='mnt_regridedges_dump')
-      ! Dump the weights to a file
+                                  & bind(C, name='mnt_regridedges_dump')
+      ! Dump interpolation weights in NetCDF file
       ! @param obj instance of mntregridedges_t (opaque handle)
       ! @param filename file name, \0 (char(0)) terminated
       ! @param n length of filename 
       ! @return 0 if successful
-      use, intrinsic :: iso_c_binding, only: c_size_t, c_int, c_double, c_ptr, c_char
+      use, intrinsic :: iso_c_binding, only: c_size_t, c_int, c_ptr, c_char
       implicit none
       type(c_ptr), intent(inout)               :: obj ! void**
       character(kind=c_char), intent(in)       :: filename(*)
       integer(c_size_t), value                 :: n
-      integer(c_int)                           :: mnt_regridedges_dump  
+      integer(c_int)                           :: mnt_regridedges_dump    
     end function mnt_regridedges_dump
 
   end interface
