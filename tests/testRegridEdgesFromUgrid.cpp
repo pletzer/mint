56ecf468fcf2c62fbe556431dbf0ac141b40e71d
Alex Pletzer <alexander@gokliya.net>
2019-03-09 11:46:29 +1300

    more checks

--- tests/testRegridEdgesFromUgrid.cpp
+++ tests/testRegridEdgesFromUgrid.cpp
@@ -35,6 +35,75 @@ void test1() {
 
 }
 
+void testSameGrid4() {
+
+    int ier;
+    std::string srcFile = "@CMAKE_SOURCE_DIR@/data/cs_4.nc";
+    std::string dstFile = "@CMAKE_SOURCE_DIR@/data/cs_4.nc";
+    std::string outputFile = "weights.nc";
+
+    RegridEdges_t* rg;
+
+    ier = mnt_regridedges_new(&rg);
+    assert(ier == 0);
+
+    ier = mnt_regridedges_loadSrc(&rg, srcFile.c_str(), (int) srcFile.size());
+    assert(ier == 0);
+    std::cerr << "testSameGrid4: loadSrc...OK\n";
+
+    ier = mnt_regridedges_loadDst(&rg, dstFile.c_str(), (int) dstFile.size());
+    assert(ier == 0);
+    std::cerr << "testSameGrid4: loadDst...OK\n";
+
+    int numCellsPerBucket = 1;
+    ier = mnt_regridedges_build(&rg, numCellsPerBucket);
+    assert(ier == 0);
+    std::cerr << "testSameGrid4: build...OK\n";
+
+    vtkIdType edgeId;
+    int edgeSign;
+    double p0[3];
+    double p1[3];
+
+    // set the source data
+    size_t numSrcCells;
+    ier = mnt_grid_getNumberOfCells(&rg->srcGridObj, &numSrcCells);
+    std::vector<double> srcData(numSrcCells * 4);
+    for (size_t cellId = 0; cellId < numSrcCells; ++cellId) {
+        for (int ie = 0; ie < 4; ++ie) {
+            ier = mnt_grid_getPoints(&rg->srcGridObj, cellId, ie, p0, p1);
+            srcData[cellId*4 + ie] = streamFunc(p1) - streamFunc(p0);
+        }
+    }
+
+    size_t numDstCells;
+    ier = mnt_grid_getNumberOfCells(&rg->dstGridObj, &numDstCells);
+    std::vector<double> dstData(numDstCells * 4);
+
+    // regrid
+    ier = mnt_regridedges_applyWeightsToCellEdgeField(&rg, &srcData[0], &dstData[0]);
+    assert(ier == 0);
+
+    // check
+    double totError = 0;
+    for (size_t dstCellId = 0; dstCellId < numDstCells; ++dstCellId) {
+        for (int ie = 0; ie < 4; ++ie) {
+            size_t k = dstCellId*4 + ie;
+            std::cerr << " dst edge Id: " << dstCellId << " edge " << ie << " dstData = " << dstData[k] << " exact: " << srcData[k] << '\n';
+            totError += std::abs(dstData[k] - srcData[k]);
+        }
+    }
+
+    std::cout << " total interpolation |error|: " << totError << '\n';
+    assert(totError < 1.e-12);
+
+    // clean up
+    ier = mnt_regridedges_del(&rg);
+    assert(ier == 0);
+
+   
+}
+
 void test2() {
 
     int ier;
@@ -66,8 +135,6 @@ void test2() {
     double p1[3];
 
     // set the source data
-    ier = mnt_grid_new(&rg->srcGridObj);
-    ier = mnt_grid_loadFrom2DUgrid(&rg->srcGridObj, srcFile.c_str());
     size_t numSrcCells, numSrcEdges;
     ier = mnt_grid_getNumberOfCells(&rg->srcGridObj, &numSrcCells);
     ier = mnt_grid_getNumberOfUniqueEdges(&rg->srcGridObj, &numSrcEdges);
@@ -81,8 +148,6 @@ void test2() {
     }
 
     // set the exact destination data
-    ier = mnt_grid_new(&rg->dstGridObj);
-    ier = mnt_grid_loadFrom2DUgrid(&rg->dstGridObj, dstFile.c_str());
     size_t numDstCells, numDstEdges;
     ier = mnt_grid_getNumberOfCells(&rg->dstGridObj, &numDstCells);
     ier = mnt_grid_getNumberOfUniqueEdges(&rg->dstGridObj, &numDstEdges);
@@ -119,6 +184,7 @@ void test2() {
 int main() {
 
     test1();
+    testSameGrid4();
     test2();
 
     return 0;
