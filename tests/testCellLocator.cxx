b43dc61584f6bee46bc48551ea58a00a5cf510a9
Alex Pletzer <alexander@gokliya.net>
2019-05-28 12:03:09 +1200

    + testing FindCell

--- tests/testCellLocator.cxx
+++ tests/testCellLocator.cxx
@@ -1,6 +1,7 @@
 #include <vmtCellLocator.h>
 #include <vtkDoubleArray.h>
 #include <vtkUnstructuredGrid.h>
+#include <vtkGenericCell.h>
 #include <vtkPoints.h>
 #include <cassert>
 #undef NDEBUG // turn on asserts
@@ -38,6 +39,26 @@ void test1Quad(int numCellsPerBucket) {
     cloc->SetDataSet(grid);
     cloc->SetNumberOfCellsPerBucket(numCellsPerBucket);
     cloc->BuildLocator();
+
+    // test 
+    vtkIdType cellId = -1;
+    double tol2 = 1.e-10;
+    vtkGenericCell* cell = NULL;
+    double pcoords[3];
+    double weights[8];
+
+    // point is inside
+    point[0] = 0.1; point[1] = 0.2; point[2] = 0.0;
+    cellId = cloc->FindCell(point, tol2, cell, pcoords, weights);
+    assert(cellId == 0);
+    std::cout << "param coords for point " << point[0] << ',' << point[1] << " are " << pcoords[0] << ',' << pcoords[1] << '\n';
+    assert(std::abs(pcoords[0] - 0.1) < tol2);
+    assert(std::abs(pcoords[1] - 0.2) < tol2);
+
+    // point is outside
+    point[0] = -0.1; point[1] = 0.2; point[2] = 0.0;
+    cellId = cloc->FindCell(point, tol2, cell, pcoords, weights);
+    assert(cellId < 0);
 
     // clean up
     cloc->Delete();
