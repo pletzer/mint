cecbec149e14bfd730365078c578ac9f10307bf5
Alex Pletzer <alexander@gokliya.net>
2019-06-03 16:27:02 +1200

    + fixes

--- tools/cellByCellStreamline.py
+++ tools/cellByCellStreamline.py
@@ -14,7 +14,7 @@ cell = vtk.vtkGenericCell()
 xsis = numpy.zeros((3,), numpy.float64)
 etas = numpy.zeros((3,), numpy.float64)
 weights = numpy.zeros((8,), numpy.float64)
-yp = numpy.zeros((2,), numpy.float64)
+yp = numpy.zeros((3,), numpy.float64)
 
 parser = argparse.ArgumentParser(description='Convert cell-by-cell edge field into an edge field')
 parser.add_argument('-i', dest='inputFile', default='res.vtk', help='Specify path to VTK, cell-by-cell input file')
@@ -22,47 +22,49 @@ parser.add_argument('-o', dest='outputFile', default='edges.vtk', help='Specify 
 parser.add_argument('-v', dest='edgeFieldName', default='edge_integrated_velocity', help='Specify name of edge integrated variable')
 parser.add_argument('-0', dest='initialPosition', default='180.0, 0.0', help='Specify initial condition "lon,lat"')
 parser.add_argument('-tf', dest='finalTime', default=100.0, type=float, help='Specify final time')
-parser.add_argument('-nt', dest='finalTime', default=100, type=int, help='Specify number of time steps')
+parser.add_argument('-nt', dest='numSteps', default=100, type=int, help='Specify number of time steps')
 
 args = parser.parse_args()
 
 
 
-def tendency(t, point, loc, grid, edgeData):
+def tendency(t, point, loc, grid):
 
-	pts = grid.GetPoints()
-	data = grid.GetCellData()
+    print('*** point = {}'.format(point))
 
-	# find the cell and the param coords xis
-	cellId = loc.FindCell(point, TOL, cell, xsis, weights)
-	if cellId < 0:
-		print('ERROR: out of domain integration')
-		return numpy.zeros((2,), numpy.float64)
+    pts = grid.GetPoints()
+    data = grid.GetCellData().GetAbstractArray(args.edgeFieldName)
 
-	# complement to xi
-	etas[:] = 1.0 - xsis
+    # find the cell and the param coords xis
+    cellId = loc.FindCell(point, TOL, cell, xsis, weights)
+    if cellId < 0:
+        print('ERROR: out of domain integration')
+        return numpy.zeros((2,), numpy.float64)
 
-	# get the corner points of the cell
-	ptPtIds = cell.GetPointIds()
-	corners = []
-	for i in range(ptPtIds.GetNumberOfIds()):
-		p = pts.GetPoint(ptIds.GetId(i))
-		corners.append( numpy.array(p[0], p[1]) )
+    # complement to xi
+    etas[:] = 1.0 - xsis
 
-	dPd0 = (p1 - p0)*etas[1] + (p2 - p3)*xsis[1]
-	dPd1 = (p3 - p0)*etas[0] + (p2 - p1)*xsis[0]
+    # get the corner points of the cell
+    ptIds = cell.GetPointIds()
+    corner = []
+    for i in range(ptIds.GetNumberOfIds()):
+        p = pts.GetPoint(ptIds.GetId(i))
+        corner.append( numpy.array([p[0], p[1]]) )
 
-	# Jacobian (cell area in lon-lat coords)
-	jac = dPd0[0]*dPd1[1] - dPd0[1]*dPd1[0]
+    dPd0 = (corner[1] - corner[0])*etas[1] + (corner[2] - corner[3])*xsis[1]
+    dPd1 = (corner[3] - corner[0])*etas[0] + (corner[2] - corner[1])*xsis[0]
 
-	edgeVals = numpy.array([data.GetTuple(cellId, i) for i in range(4)])
+    # Jacobian (cell area in lon-lat coords)
+    jac = dPd0[0]*dPd1[1] - dPd0[1]*dPd1[0]
 
-	yp[0] = ( + (edgeVals[0]*etas[1] + edgeVals[2]*xsis[1])*dPd1[1] 
-		      - (edgeVals[3]*etas[0] + edgeVals[1]*xsis[0])*dPd0[1] )/jac
-	yp[1] = ( - (edgeVals[0]*etas[1] + edgeVals[2]*xsis[1])*dPd1[0] 
-		      + (edgeVals[3]*etas[0] + edgeVals[1]*xsis[0])*dPd0[0] )/jac
+    edgeVals = numpy.array([data.GetTuple(cellId, i) for i in range(4)])
 
-	return yp
+    yp[0] = ( + (edgeVals[0]*etas[1] + edgeVals[2]*xsis[1])*dPd1[1] 
+              - (edgeVals[3]*etas[0] + edgeVals[1]*xsis[0])*dPd0[1] )/jac
+    yp[1] = ( - (edgeVals[0]*etas[1] + edgeVals[2]*xsis[1])*dPd1[0] 
+              + (edgeVals[3]*etas[0] + edgeVals[1]*xsis[0])*dPd0[0] )/jac
+
+    return yp
 
 # read the file 
 reader = vtk.vtkUnstructuredGridReader()
@@ -74,12 +76,13 @@ ugrid = reader.GetOutput()
 
 # create a locator
 loc = vtk.vtkCellLocator()
-loc.SetDataSet(grid)
+loc.SetDataSet(ugrid)
 loc.BuildLocator()
 
-edgeData = ugrid.GetCellData().GetAbstractArray(args.edgeFieldName)
 
-p0 = numpy.array(eval(args.initialPosition))
-sol = odeint(tendency, p0, 0.0, args=(loc, ugrid, edgeData))
+p0 = numpy.array(list(eval(args.initialPosition)) + [0.0])
+print(p0)
+timeSteps = numpy.linspace(0.0, args.finalTime, args.numSteps + 1)
+sol = odeint(tendency, p0, timeSteps, tfirst=True, args=(loc, ugrid))
 
 print(sol)
