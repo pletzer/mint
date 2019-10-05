38c6fbb838b531756cfbaf38ad8ebc9dc4eb0d3b
Alex Pletzer <alexander@gokliya.net>
2019-10-05 16:15:13 +1300

    now also storing the upstream advected coords

--- tools/generate_point_velocity.py
+++ tools/generate_point_velocity.py
@@ -4,6 +4,8 @@ import argparse
 import sys
 import time
 import functools
+from scipy.integrate import odeint
+
 
 parser = argparse.ArgumentParser(description='Generate point velocity')
 parser.add_argument('-u', dest='velocityX', default='0.01*sin(pi*x/180.)',
@@ -14,66 +16,112 @@ parser.add_argument('-g', dest='grid_file', default='',
                     help='Specify the netcdf file containing the grid geometry/topology')
 parser.add_argument('-N', dest='grid_var', default='', 
                     help='Specify the grid variable name in the netcdf file')
-parser.add_argument('-d', dest='data_file', default='', 
-                    help='Specify the netcdf file containing the edge integrated velocity data')
+parser.add_argument('-tf', dest='finalTime', default=1.0, type=float,
+                    help='Specify final time for integrating trajectories upstream')
+parser.add_argument('-d', dest='upstream_data_file', default='', 
+                    help='Specify the netcdf file containing the upstream coordinates and the velocity')
 args = parser.parse_args()
 
 if len(args.grid_file) == 0:
     print('ERROR: must provide grid file (-g)')
     sys.exit(1)
 
-if len(args.data_file) == 0:
-    print('ERROR: must provide data file (-d)')
+if len(args.upstream_data_file) == 0:
+    print('ERROR: must provide upstream data file (-d)')
     sys.exit(2)
+
+if args.upstream_data_file == args.grid_file:
+    print('ERROR: upstream data file name must be different from grid file name')
+    sys.exit(3)
 
 if len(args.grid_var) == 0:
     print('ERROR: must provide a grid variable name (-N)')
-    sys.exit(3)
+    sys.exit(4)
 
 
-nc = netCDF4.Dataset(args.grid_file, 'r')
+ncGrid = netCDF4.Dataset(args.grid_file, 'r')
 
 # get the vertex coordinate names
-xname, yname = nc[args.grid_var].node_coordinates.split(' ')
+xName, yName = ncGrid[args.grid_var].node_coordinates.split(' ')
 
-# read the coordinates
-x = nc.variables[xname][:]
-y = nc.variables[yname][:]
+# get grid dimensions
+numPoints = ncGrid[xName].shape[0]
+numEdges = ncGrid[ncGrid[args.grid_var].edge_node_connectivity].shape[0]
+numFaces = ncGrid[ncGrid[args.grid_var].face_node_connectivity].shape[0]
 
-numPointDimName = nc.variables[xname].dimensions[0]
+# read the coordinates (initial conditions)
+x = ncGrid.variables[xName][:]
+y = ncGrid.variables[yName][:]
 
-nc.close()
-
-# write the velocity to disk
-if args.data_file == args.grid_file:
-    # append to grid file
-    print('NOTE: point velocity field appended to grid file {}'.format(args.grid_file))
-    nc = netCDF4.Dataset(args.data_file, 'r+')
-else:
-    # new file
-    print('NOTE: point velocity field saved in {}'.format(args.data_file))
-    nc = netCDF4.Dataset(args.data_file, 'w')
+# open the output file
+ncUp = netCDF4.Dataset(args.upstream_data_file, 'w')
 # write global attributes
-nc.date = 'Created on {}'.format(time.asctime())
-nc.command = functools.reduce(lambda x, y: x+' '+y, sys.argv)
+ncUp.date = 'Created on {}'.format(time.asctime())
+ncUp.command = functools.reduce(lambda x, y: x+' '+y, sys.argv)
 
-numPoints = x.shape[0]
-twoDimName = 'velocity_num_components'
+# create dimensions and variables
+twoDimName = 'two'
+fourDimName = 'four'
+numPointsDimName = 'num_points'
+numEdgesDimName = 'num_edges'
+numFacesDimName = 'num_faces'
+ncUp.createDimension(twoDimName, 2)
+ncUp.createDimension(fourDimName, 4)
+ncUp.createDimension(numPointsDimName, numPoints)
+ncUp.createDimension(numEdgesDimName, numEdges)
+ncUp.createDimension(numFacesDimName, numFaces)
 
-# create dienmsions (unless there is already a dimension with the same name)
-try:
-    nc.createDimension(twoDimName, 2)
-except:
-    pass
-try:
-    nc.createDimension(numPointDimName, numPoints)
-except:
-    pass
-velocity = nc.createVariable('velocity', 'f8', (numPointDimName, twoDimName))
+# copy the topology over
+gridVarUpName = args.grid_var + '_upstream'
+gridVarUp = ncUp.createVariable(gridVarUpName, 'i4')
+# save upstream grid
+for attrName in ncGrid[args.grid_var].ncattrs():
+    attrVal = getattr(ncGrid[args.grid_var], attrName)
+    setattr(gridVarUp, attrName, attrVal)
+# new coordinates
+xNameUp = xName + '_upstream'
+yNameUp = yName + '_upstream'
+gridVarUp.node_coordinates = '{} {}'.format(xNameUp, yNameUp)
+
+faceNodeUp = ncUp.createVariable(gridVarUp.face_node_connectivity, 'i4', 
+                               (numFacesDimName, fourDimName))
+faceEdgeUp = ncUp.createVariable(gridVarUp.face_edge_connectivity, 'i4',
+                               (numFacesDimName, fourDimName))
+EdgeNodeUp = ncUp.createVariable(gridVarUp.edge_node_connectivity, 'i4',
+                               (numEdgesDimName, twoDimName))
+
+velocity = ncUp.createVariable('velocity', 'f8', (numPointsDimName, twoDimName))
 velocity.location = 'node'
 
+# velocity at nodes
 from numpy import sin, cos, pi
 velocity[:, 0] = eval(args.velocityX)
 velocity[:, 1] = eval(args.velocityY)
-nc.close()
+
+# integrate the nodal positions backwards in time
+vxy = numpy.zeros((numPoints*2,), numpy.float64)
+def tendency(xy, t):
+    x = xy[:numPoints]
+    y = xy[numPoints:]
+    vxy[:numPoints] = eval(args.velocityX)
+    vxy[numPoints:] = eval(args.velocityY)
+    return vxy
+
+xy = numpy.zeros((numPoints*2,), numpy.float64)
+xy[:numPoints] = x
+xy[numPoints:] = y
+xyUpstream = odeint(tendency, xy, [0.0, -args.finalTime])
+xUpstream = xyUpstream[1, :numPoints]
+yUpstream = xyUpstream[1, numPoints:]
+
+
+# save the new coordinates
+xVarUp = ncUp.createVariable(xNameUp, 'f8', (numPointsDimName,))
+yVarUp = ncUp.createVariable(yNameUp, 'f8', (numPointsDimName,))
+# write
+xVarUp[:] = xUpstream
+yVarUp[:] = yUpstream
+
+ncGrid.close()
+ncUp.close()
 
