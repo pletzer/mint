82e05d2c869e5eb40e0a1dff4521a20a6a4745a2
Alex Pletzer <alexander@gokliya.net>
2019-11-08 19:06:16 +1100

    commenting out the prints

--- tools/cellByCell2Edges.py
+++ tools/cellByCell2Edges.py
@@ -53,6 +53,7 @@ for icell in range(ncells):
 		edata.SetTuple(iedge, (sign * data.GetComponent(icell, ie),))
 
 
+		"""
 		ptId0, ptId1 = edgePtIds.GetId(0), edgePtIds.GetId(1)
 		pts = ugrid.GetPoints()
 		p0 = pts.GetPoint(ptId0)
@@ -60,6 +61,7 @@ for icell in range(ncells):
 		d = sign * data.GetComponent(icell, ie)
 		print('setting edge value iedge={} for icell={} and ie={} to {} ptIds: {} -> {} pts: {} -> {}'.format(iedge, \
 			     icell, ie, d, ptId0, ptId1, p0, p1))
+		"""
 
 
 		egrid.InsertNextCell(vtk.VTK_LINE, edgePtIds)
