3b30a61923a17cb92d9b6f57a9f4867fd597faa0
Alex Pletzer <alexander@gokliya.net>
2020-02-09 13:53:11 +1300

    + capi for mnt_ncfieldread

--- src/mnt_ncfieldread_capi_mod.f90
+++ src/mnt_ncfieldread_capi_mod.f90
@@ -28,6 +28,57 @@ module mnt_ncfieldread_capi_mod
       integer(c_int)                   :: mnt_ncfieldread_del
     end function mnt_ncfieldread_del
 
+    
+    function mnt_ncfieldread_getNumDims(obj, ndims) &
+                                        bind(C, name='mnt_ncfieldread_getNumDims')
+      use, intrinsic :: iso_c_binding, only: c_int, c_ptr
+      implicit none
+      type(c_ptr), intent(inout)       :: obj ! void**
+      integer(c_int)                   :: ndims
+      integer(c_int)                   :: mnt_ncfieldread_getNumDims
+    end function mnt_ncfieldread_getNumDims
+
+    function mnt_ncfieldread_getDimName(obj, iAxis, dimName, dimNameLen) &
+                                        bind(C, name='mnt_ncfieldread_getDimName')
+      use, intrinsic :: iso_c_binding, only: c_int, c_ptr
+      implicit none
+      type(c_ptr), intent(inout)       :: obj ! void**
+      integer(c_int), intent(in)       :: iAxis
+      character(len=1), intent(out)    :: dimName
+      integer(c_int), value            :: dimNameLen
+      integer(c_int)                   :: mnt_ncfieldread_getDimName
+    end function mnt_ncfieldread_getDimName
+
+    function mnt_ncfieldread_getDim(obj, iAxis, dim) &
+                                        bind(C, name='mnt_ncfieldread_getDim')
+      use, intrinsic :: iso_c_binding, only: c_int, c_ptr
+      implicit none
+      type(c_ptr), intent(inout)       :: obj ! void**
+      integer(c_int), value            :: iAxis
+      integer(c_int), intent(out)      :: dim
+      integer(c_int)                   :: mnt_ncfieldread_getDim
+    end function mnt_ncfieldread_getDim
+
+    function mnt_ncfieldread_readData(obj, data) &
+                                        bind(C, name='mnt_ncfieldread_readData')
+      use, intrinsic :: iso_c_binding, only: c_int, c_ptr, c_double
+      implicit none
+      type(c_ptr), intent(inout)       :: obj ! void**
+      real(c_double), intent(out)      :: data
+      integer(c_int)                   :: mnt_ncfieldread_readData
+    end function mnt_ncfieldread_readData
+
+    function mnt_ncfieldread_readDataSlice(obj, startInds0, counts, data) &
+                                        bind(C, name='mnt_ncfieldread_readDataSlice')
+      use, intrinsic :: iso_c_binding, only: c_int, c_ptr, c_double, c_size_t
+      implicit none
+      type(c_ptr), intent(inout)       :: obj ! void**
+      integer(c_size_t), intent(in)    :: startInds0(*)
+      integer(c_size_t), intent(in)    :: counts(*)
+      real(c_double), intent(out)      :: data
+      integer(c_int)                   :: mnt_ncfieldread_readDataSlice
+    end function mnt_ncfieldread_readDataSlice   
+
   end interface
 
 end module mnt_ncfieldread_capi_mod
