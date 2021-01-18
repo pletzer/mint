49e91d04998831a3789510c63a84494660e19a07
Alex Pletzer <alexander@gokliya.net>
2021-01-18 14:19:27 +1300

    + docstrings

--- mint/regrid_edges.py
+++ mint/regrid_edges.py
@@ -8,6 +8,9 @@ class RegridEdges(object):
 
 
     def __init__(self):
+        """
+        Regrid edge field constructor.
+        """
 
         self.regrid_obj = byref(c_void_p())
 
@@ -17,12 +20,25 @@ class RegridEdges(object):
 
 
     def __del__(self):
+        """
+        Regrid edge field destructor.
+        """
         ier = LIB.mnt_regridedges_del(self.regrid_obj)
         if ier: 
             error_handler('regrid_edges.py', '__del__', ier)
 
 
     def setSrcGridFlags(self, fixLonAcrossDateline, averageLonAtPole):
+        """
+        Set the source grid flags.
+
+        :param fixLonAcrossDateline: set to 1 if a periodicity length should be added/subtracted
+                                     in order to make eeach cell as compact as poassible
+        :param averageLonAtPole: set to 1 if the longitudes at the poles should the average of 
+                                 the cell's longitudes
+
+        note:: a lon-lat grid requires 0, 0 and a cibed sphere grid requires 1, 1
+        """
         LIB.mnt_regridedges_setSrcGridFlags.argtypes = [POINTER(c_void_p), c_int, c_int]
         ier = LIB.mnt_regridedges_setSrcGridFlags(self.regrid_obj, fixLonAcrossDateline, averageLonAtPole)
         if ier:
@@ -30,6 +46,16 @@ class RegridEdges(object):
 
 
     def setDstGridFlags(self, fixLonAcrossDateline, averageLonAtPole):
+        """
+        Set the destrination grid flags.
+
+        :param fixLonAcrossDateline: set to 1 if a periodicity length should be added/subtracted
+                                     in order to make eeach cell as compact as poassible
+        :param averageLonAtPole: set to 1 if the longitudes at the poles should the average of 
+                                 the cell's longitudes
+
+        note:: a lon-lat grid requires 0, 0 and a cibed sphere grid requires 1, 1
+        """
         LIB.mnt_regridedges_setDstGridFlags.argtypes = [POINTER(c_void_p), c_int, c_int]
         ier = LIB.mnt_regridedges_setDstGridFlags(self.regrid_obj, fixLonAcrossDateline, averageLonAtPole)
         if ier:
@@ -37,6 +63,11 @@ class RegridEdges(object):
 
 
     def loadSrcGrid(self, filename):
+        """
+        Load a source grid from a 2D UGRID file.
+
+        :param filename: string in the format filename:meshname
+        """
         LIB.mnt_regridedges_loadSrcGrid.argtypes = [POINTER(c_void_p), c_char_p, c_int]
         ier = LIB.mnt_regridedges_loadSrcGrid(self.regrid_obj, filename, len(filename))
         if ier:
@@ -44,6 +75,11 @@ class RegridEdges(object):
 
 
     def loadDstGrid(self, filename):
+        """
+        Load a destination grid from a 2D UGRID file.
+
+        :param filename: string in the format filename:meshname
+        """
         LIB.mnt_regridedges_loadDstGrid.argtypes = [POINTER(c_void_p), c_char_p, c_int]
         ier = LIB.mnt_regridedges_loadDstGrid(self.regrid_obj, filename, len(filename))
         if ier:
@@ -51,6 +87,13 @@ class RegridEdges(object):
 
 
     def build(self, numCellsPerBucket, periodX, debug):
+        """
+        Build the regridder and compute the regridding weights
+
+        :param numCellsPerBucket: average number of cells per bucket (affects performance only)
+        :param periodX: periodicity length (set to 0 if non-periodic)
+        :param debug: 0=no debug info, 1=print debug info, 2=save bad edges in VTK file
+        """
         LIB.mnt_regridedges_build.argtypes = [POINTER(c_void_p), c_int, c_double, c_int]
         ier = LIB.mnt_regridedges_build(self.regrid_obj, numCellsPerBucket, periodX, debug)
         if ier:
@@ -58,6 +101,10 @@ class RegridEdges(object):
 
 
     def dumpWeights(self, filename):
+        """
+        Dump the weights to a file
+        :param filename: file name
+        """
         LIB.mnt_regridedges_dumpWeights.argtypes = [POINTER(c_void_p), c_char_p, c_int]
         ier = LIB.mnt_regridedges_dumpWeights(self.regrid_obj, filename, len(filename))
         if ier:
@@ -65,6 +112,10 @@ class RegridEdges(object):
 
 
     def loadWeights(self, filename):
+        """
+        Load the weights from a file
+        :param filename: file name
+        """
         LIB.mnt_regridedges_loadWeights.argtypes = [POINTER(c_void_p), c_char_p, c_int]
         ier = LIB.mnt_regridedges_loadWeights(self.regrid_obj, filename, len(filename))
         if ier:
@@ -72,6 +123,12 @@ class RegridEdges(object):
 
 
     def apply(self, srcdata, dstdata):
+        """
+        Apply the regridding weights to an edge field with unique edge Ids
+
+        :param srcdata: contiguous arrays of source field data
+        :param dstdata: contiguous arrays of destination field data (will be filled in)
+        """
         LIB.mnt_regridedges_apply.argtypes = [POINTER(c_void_p), 
                                               numpy.ctypeslib.ndpointer(dtype=numpy.float64), 
                                               numpy.ctypeslib.ndpointer(dtype=numpy.float64)]
