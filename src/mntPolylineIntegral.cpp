5a8326bb87b424939e6ac9163db7af01cbf782ed
Alex Pletzer <alexander@gokliya.net>
2021-01-22 13:25:59 +1300

    mntPolylineIntegral now compiles

---
+++ src/mntPolylineIntegral.cpp
@@ -0,0 +1,144 @@
+#include <mntPolylineIntegral.h>
+#include <mntPolysegmentIter.h>
+#include <iostream>
+
+extern "C"
+int mnt_Polylineintegral_new(PolylineIntegral_t** self) {
+    
+    *self = new PolylineIntegral_t();
+    (*self)->grid = NULL;
+
+    return 0;
+}
+
+extern "C"
+int mnt_Polylineintegral_del(PolylineIntegral_t** self) {
+    int ier = 0;
+
+    // destroy everything here
+
+    delete *self;
+    return ier;
+}
+
+extern "C"
+int mnt_Polylineintegral_setGrid(PolylineIntegral_t** self, Grid_t* grid) {
+    (*self)->grid = grid;
+    return 0;
+}
+
+extern "C"
+int mnt_Polylineintegral_setPolyline(PolylineIntegral_t** self, int npoints, const double xyz[]) {
+    (*self)->lineXYZ.resize(3 * npoints);
+    // copy the line
+    for (auto i = 0; i < npoints*3; ++i) {
+        (*self)->lineXYZ[i] = xyz[i]; // copy
+    }
+    return 0;
+}
+
+extern "C"
+int mnt_Polylineintegral_build(PolylineIntegral_t** self) {
+
+    if ((*self)->lineXYZ.size() == 0) {
+        std::cerr << "ERROR: need to call setPolyline before calling build\n";
+        return 1;
+    }
+
+    if (!(*self)->grid) {
+        std::cerr << "ERROR: need to call setGrid before calling build\n";
+        return 2;
+    }
+
+    vtkUnstructuredGrid* vgrid = (*self)->grid->grid;
+
+    // build the cell locator
+    vmtCellLocator* loc = vmtCellLocator::New();
+    loc->SetDataSet(vgrid);
+    loc->BuildLocator();
+
+    // number of points/vertices (3D)
+    size_t npts = (*self)->lineXYZ.size() / 3;
+
+    // iterate over the Polyline segments
+    for (size_t ip0 = 0; ip0 < npts - 1; ++ip0) {
+
+        // get the starting point
+        size_t k0 = 3*ip0;
+        double p0[] = {(*self)->lineXYZ[k0 + 0], (*self)->lineXYZ[k0 + 1], (*self)->lineXYZ[k0 + 2]};
+
+        // get the end point
+        size_t k1 = k0 + 3;        
+        double p1[] = {(*self)->lineXYZ[k1 + 0], (*self)->lineXYZ[k1 + 1], (*self)->lineXYZ[k1 + 2]};
+
+        // build the polysegment iterator. This breaks segment p0 -> p1 into 
+        // smaller segments, each being entirely contained within grid cells
+        PolysegmentIter polyseg(vgrid, loc, p0, p1);
+
+        // number of sub-segments
+        size_t numSubSegs = polyseg.getNumberOfSegments();
+        polyseg.reset();
+
+        // iterate over the sub-segments 
+        for (size_t i = 0; i < numSubSegs; ++i) {
+
+            // get the cell Id to which this sub-segment belongs to
+            vtkIdType cellId = polyseg.getCellId();
+
+            // get the start/end 1D parameter values for this sub-segment
+            double ta = polyseg.getBegLineParamCoord();
+            double tb = polyseg.getEndLineParamCoord();
+
+            // get the cell parametric start/end values for the sub-segment
+            const Vec3& xia = polyseg.getBegCellParamCoord();
+            const Vec3& xib = polyseg.getEndCellParamCoord();
+
+            // some sub-segments belong to two or more cells (e.g. if the sub-segment
+            // runs along a cell edge)
+            double coeff = polyseg.getCoefficient();
+
+            // 2D because our cells are quads
+            std::vector<double> dxi({xib[0] - xia[0], xib[1] - xia[1]});
+            std::vector<double> xiMid({0.5*(xia[0] + xib[0]), 0.5*(xia[1] + xib[1])});
+
+            // compute the 4 weight contributions from each cell edge
+            double ws[] = {+ dxi[0] * (1.0 - xiMid[1]) * coeff,
+                           + dxi[1] * (0.0 + xiMid[0]) * coeff,
+                           - dxi[0] * (0.0 + xiMid[1]) * coeff,
+                           - dxi[1] * (1.0 - xiMid[0]) * coeff};
+
+            // store the weights for this sub-segment
+            std::pair<vtkIdType, int> cIdE;
+            for (int edgeIndex = 0; edgeIndex < 4; ++edgeIndex) {
+                cIdE = std::pair<vtkIdType, int>(cellId, edgeIndex);
+                (*self)->weights.insert(std::pair< std::pair<vtkIdType, int>, double>(cIdE, ws[edgeIndex]));
+            }            
+        }
+    }
+
+    loc->Delete();
+
+    return 0;
+}
+
+extern "C"
+int mnt_Polylineintegral_getIntegral(PolylineIntegral_t** self, const double data[], double& result) {
+
+    result = 0;
+    for (auto it = (*self)->weights.cbegin(); it != (*self)->weights.cend(); ++it) {
+
+        // get the cell Id
+        vtkIdType cellId = it->first.first;
+
+        // get the edge index
+        int edgeIndex = it->first.second;
+
+        // get the weight
+        double wght = it->second;
+
+        // add the contibution
+        result += wght * data[cellId*4 + edgeIndex];
+    }
+
+    return 0;
+}
---
+++ src/mntPolylineIntegral.h
@@ -0,0 +1,90 @@
+
+#include <vector>
+#include <map>
+#include <string>
+#include <vtkUnstructuredGrid.h>
+#include <vmtCellLocator.h>
+#include <mntGrid.h>
+#include <mntQuadEdgeIter.h>
+#include <mntNcAttributes.h>
+#include <mntNcFieldRead.h>
+#include <mntNcFieldWrite.h>
+#include <mntMultiArrayIter.h>
+
+#ifndef MNT_Polyline_INTEGRAL
+#define MNT_Polyline_INTEGRAL
+
+/**
+ * @brief Edge-centred field regridding
+ *
+ * A class to compute the regridding weights of an edge-centred field
+ */
+
+struct PolylineIntegral_t {
+
+    /** pointer to the grid */
+    Grid_t* grid;
+
+    /** line coordinates */
+    std::vector<double> lineXYZ;
+
+    /** interpolation weights map: (cellId, edgeIndex) -> weight */
+    std::map< std::pair<vtkIdType, int>, double > weights;
+
+};
+
+/**
+ * Constructs a regridding object for edge centred fields
+ * @param self instance of the regridding object
+ * @return error code (0 is OK)
+ */
+extern "C"
+int mnt_Polylineintegral_new(PolylineIntegral_t** self);
+
+/**
+ * Destructor
+ * @param self this instance
+ * @return error code (0 is OK)
+ */
+extern "C"
+int mnt_Polylineintegral_del(PolylineIntegral_t** self);
+
+/**
+ * Set the grid
+ * @param self instance of the regridding object'
+ * @param grid instance of Grid_t
+ * @return error code (0 = OK)
+ */
+extern "C"
+int mnt_Polylineintegral_setGrid(PolylineIntegral_t** self, Grid_t* grid);
+
+/**
+ * Set the target Polyline 
+ * @param self instance of the regridding object
+ * @param npoints number of points
+ * @param xyz flat array of xyz coordinates
+ * @return error code (0 = OK)
+ */
+extern "C"
+int mnt_Polylineintegral_setPolyline(PolylineIntegral_t** self, int npoints, const double xyz[]);
+
+/** 
+ * Build the interpolation
+ * @param self instance of the regridding object
+ * @return error code (0 is OK)
+ */
+extern "C"
+int mnt_Polylineintegral_build(PolylineIntegral_t** self);
+
+/** 
+ * Get the integral of the field along the line
+ * @param self instance of the regridding object
+ * @param data the field values integrated over the grid's edges
+ * @param result line integral of the filled over the Polyline
+ * @return error code (0 is OK)
+ */
+extern "C"
+int mnt_Polylineintegral_getIntegral(PolylineIntegral_t** self, const double data[], double& result);
+
+
+#endif // MNT_POLY_LINE_INTEGRAL
