d6fefd50e5d662ee4c8b922bb46a3bfe8a3b930c
Alex Pletzer <alexander@gokliya.net>
2021-05-17 15:27:17 +1200

    cleaning the grid test

--- mint/tests/test_grid.py
+++ mint/tests/test_grid.py
@@ -47,51 +47,39 @@ def test_attach_data():
     assert(arr.GetNumberOfTuples() == gr.getNumberOfCells())
     assert(arr.GetNumberOfComponents() == nDataPerCell)
 
-
 def test_load_grid():
-
     gr = Grid()
-
     gr.load('test_create_grid.vtk')
-
     ncells = gr.getNumberOfCells()
     print(f'ncells = {ncells}')
-    assert ncells == 2
-
+    assert(ncells == 2)
 
 def test_load_from_ugrid_file():
-
     data_dir = Path(__file__).absolute().parent / '../../data'
     gr = Grid()
     gr.setFlags(1, 1)
     filename = str(data_dir / Path('cs_4.nc'))
     gr.loadFrom2DUgrid(f'{filename}:physics')
-
     nedges = gr.getNumberOfEdges()
     print(f'nedges = {nedges}')
-    assert nedges == 192
-
+    assert(nedges == 192)
     ncells = gr.getNumberOfCells()
     for icell in range(ncells):
         for iedge in range(4):
             edgeId, edgeSign = gr.getEdgeId(icell, iedge)
             nodeIds = gr.getNodeIds(icell, iedge)
             print(f'cell {icell} edge {iedge}: edgeId = {edgeId}, {edgeSign} nodeIds = {nodeIds}')
-
     # attaching a 3 components field to the grid
     data = numpy.array(range(ncells*4*3), numpy.float64)
     gr.attach('myData', data)
 
-
 def test_edge_arc_lengths():
-
     data_dir = Path(__file__).absolute().parent / '../../data'
     gr = Grid()
     gr.setFlags(1, 1)
     filename = str(data_dir / Path('cs_4.nc'))
     gr.loadFrom2DUgrid(f'{filename}:physics')
     gr.computeEdgeArcLengths()
-
     ncells = gr.getNumberOfCells()
     for icell in range(ncells):
         for edgeIndex in range(4):
