89648a7d923ebf6d043cc8d387ea734a938f1025
Alex Pletzer <alexander@gokliya.net>
2021-09-29 14:14:13 +1300

    trying brew to run ctest

--- docs/html/search/all_4.js
+++ docs/html/search/all_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['grid_16',['grid',['../struct_grid__t.html#af452706a6733733dad795a72c56ad176',1,'Grid_t']]],
-  ['grid_5ft_17',['Grid_t',['../struct_grid__t.html',1,'']]]
+  ['grid_0',['grid',['../struct_grid__t.html#af452706a6733733dad795a72c56ad176',1,'Grid_t']]],
+  ['grid_5ft_1',['Grid_t',['../struct_grid__t.html',1,'']]]
 ];
