32614ed6c8fb559dbca48db65ff990190133d84a
Alex Pletzer <alexander@gokliya.net>
2021-12-20 14:46:43 +1300

    work in progress

--- tools/lfriz.py
+++ tools/lfriz.py
@@ -2,6 +2,9 @@ import numpy
 import defopt
 import mint
 import netCDF4
+import vtk
+
+A_EARTH = 6371e3 # metres
 
 class LFRiz(object):
 
@@ -11,23 +14,47 @@ class LFRiz(object):
         self.srcGrid = mint.Grid()
         # cubed-sphere
         self.srcGrid.setFlags(fixLonAcrossDateline=1, averageLonAtPole=1)
-        self.srcGrid.loadFromUgrid2D(infile + inmesh)
+        self.srcGrid.loadFromUgrid2D(infile + '$' + inmesh)
 
-        # associate fluxes to each edge
+        # read the u, v components, in m/s
         nc = netCDF4.Dataset(infile)
-        u1 = nc.variables['u1'][:]
-        u2 = nc.variables['u2'][:]
+        u1 = nc.variables['u1'][tindex, level, :]
+        u2 = nc.variables['u2'][tindex, level, :]
 
+        # read the edge to node connectivity
+        e2nname = nc.variables[inmesh].edge_node_connectivity
+        e2n = nc.variables[e2nname][:]
+
+        # read the coordinates
+        xname, yname = nc.variables[inmesh].node_coordinates.split(' ')
+        lon = nc.variables[xname][:]
+        lat = nc.variables[yname][:]
+
+        # compute the fluxes in m^2/s across each edge
+        uvec = numpy.zeros((2,), numpy.float64)
+        self.influxes = numpy.zeros((self.srcGrid.getNumberOfCells(), 4), numpy.float64)
+        for icell in range(self.srcGrid.getNumberOfCells()):
+            for ie in range(4):
+                edgeIndex, _ = self.srcGrid.getEdgeId(icell, ie)
+                n0, n1 = self.srcGrid.getNodeIds(icell, ie)
+                lon0, lat0 = lon[n0], lat[n0]
+                lon1, lat1 = lon[n1], lat[n1]
+                latmid = 0.5*(lat0 + lat1)
+                dlon, dlat = lon1 - lon0, lat1 - lat0
+                self.influxes[icell, ie] = A_EARTH*(u1[edgeIndex]*dlat - u2[edgeIndex]*numpy.cos(latmid*numpy.pi/180.)*dlon)
+
+        # create structured grids with nodal flux data attached
+        numRibbons, nt = pts.shape[0] - 1, ndays + 1
+        assert(numRibbons >= 1)
+        self.ribbons = [numpy.zeros((2, nt), numpy.float64) for i in range(numRibbons)]
+        self.fields = [numpy.zeros((2, nt), numpy.float64) for i in range(numRibbons)]
+
+
+    def advect(self):
         # create a vector field interpolator
         self.vectInterp = mint.VectorInterp()
         self.vectInterp.setGrid(self.srcGrid)
         self.vectInterp.buildLocator(numCellsPerBucket=100, periodX=360.)
-
-        # create structured grids
-
-
-    def advect(self):
-        pass
 
 
     def show(self):
@@ -40,7 +67,7 @@ class LFRiz(object):
 ###############################################################################
 
 def main(*,
-         infile: str = '../data/lfric_diag_wind.nc', inmesh: str = '$Mesh2d',
+         infile: str = '../data/lfric_diag_wind.nc', inmesh: str = 'Mesh2d',
          pts: str="(0., 10.), (50., 10.)", 
          ndays : float=10, 
          tindex : int = 0, level: int = 0, 
