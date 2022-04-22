789c519743741550db2b366d260db881620a0c88
Alex Pletzer <alexander@gokliya.net>
2022-04-23 08:01:40 +1200

    fixed idenitity regrid test

--- tests/testRegridEdgesTriangles.cxx
+++ tests/testRegridEdgesTriangles.cxx
@@ -7,6 +7,122 @@
 
 #define EPS 0.5
 
+void test2Triangles2() {
+    
+    /* 2 almost triangular cells
+
+     5<2.4.....<6......3
+     :    \            :
+     v     \           ^
+     3      \          5
+     :       ^         :
+     :        1        :
+     :         \       :
+     :          \      :
+     :           \     :
+     :            \    :
+     0.......0>....1.4>2
+    */
+
+    std::size_t ncells = 2;
+    std::size_t nedges = 7;
+    std::size_t npoints = 6;
+
+    std::vector<double> xyz{0.    ,0.,0.,
+                            1.-EPS,0.,0.,
+                            1.    ,0.,0.,
+                            1.    ,1.,0.,
+                            EPS   ,1.,0.,
+                            0.    ,1.,0.};
+
+    std::vector<std::size_t> face2nodes{0, 1, 4, 5,
+                                        2, 3, 4, 1};
+
+    std::vector<std::size_t> edge2nodes{0, 1,
+                                        1, 4,
+                                        4, 5,
+                                        5, 0,
+                                        1, 2,
+                                        2, 3,
+                                        3, 4};
+
+    int ier;
+
+    Grid_t* srcGrd;
+    ier = mnt_grid_new(&srcGrd);
+    assert(ier == 0);
+    ier = mnt_grid_loadFromUgrid2DData(&srcGrd, ncells, nedges, npoints, &xyz[0], &face2nodes[0], &edge2nodes[0]);
+    assert(ier == 0);
+
+    Grid_t* dstGrd;
+    ier = mnt_grid_new(&dstGrd);
+    assert(ier == 0);
+    ier = mnt_grid_loadFromUgrid2DData(&dstGrd, ncells, nedges, npoints, &xyz[0], &face2nodes[0], &edge2nodes[0]);
+    assert(ier == 0);
+
+    RegridEdges_t* rg = NULL;
+    ier = mnt_regridedges_new(&rg);
+    assert(ier == 0);
+
+    // src and dst grids are the same
+    ier = mnt_regridedges_setSrcGrid(&rg, srcGrd);
+    assert(ier == 0);
+    ier = mnt_regridedges_setDstGrid(&rg, dstGrd);
+    assert(ier == 0);
+
+    int num_cells_per_bucket = 128;
+    double periodX = 0.;
+    int enableFolding = 0;
+    ier = mnt_regridedges_buildLocator(&rg, num_cells_per_bucket, periodX, enableFolding);
+    assert(ier == 0);
+
+    int debug = 2;
+    ier = mnt_regridedges_computeWeights(&rg, debug);
+    assert(ier == 0);
+
+    // data values are the edge indices
+    std::size_t numEdges;
+    ier = mnt_regridedges_getNumSrcEdges(&rg, &numEdges);
+    assert(ier == 0);
+
+    // set the edge field integrals to the edge Ids
+    std::vector<double> srcData(numEdges);
+    std::vector<double> dstData(numEdges);
+    for (auto i = 0; i < numEdges; ++i) {
+        srcData[i] = i;
+    }
+    // set the fluxes to zero on the small edges
+    srcData[2] = 0;
+    srcData[4] = 0;
+
+    // apply the weights
+    int placement = MNT_UNIQUE_EDGE_DATA;
+    ier = mnt_regridedges_apply(&rg, &srcData[0], &dstData[0], placement);
+    assert(ier == 0);
+
+    // check
+    double error = 0;
+    for (auto i = 0; i < numEdges; ++i) {
+        error += std::abs(dstData[i] - srcData[i]);
+        std::cout << i << " srcData = " << srcData[i] << " dstData = " << dstData[i] << '\n';
+    }
+    error /= (double) numEdges;
+
+    std::cerr << "2 triangles/2 grids error: " << error << '\n';
+    assert(error < 1.e-10);
+   
+    // reclaim the memory
+    ier = mnt_regridedges_del(&rg);
+    assert(ier == 0);
+
+    ier = mnt_grid_del(&srcGrd);
+    assert(ier == 0);
+
+    ier = mnt_grid_del(&dstGrd);
+    assert(ier == 0);
+}
+
+
 void test2Triangles() {
     
     int ier;
@@ -206,6 +322,6 @@ void test1Quad() {
 }
 
 int main() {
-    test2Triangles();
+    test2Triangles2();
     test1Quad();
 }
