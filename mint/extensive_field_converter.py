e48d75ab638e2518682a0445194ca57385e5383e
Alex Pletzer <alexander@gokliya.net>
2022-07-01 10:23:40 +1200

    fix

--- mint/extensive_field_converter.py
+++ mint/extensive_field_converter.py
@@ -5,13 +5,13 @@ from . import error_handler, warning_handler
 import numpy
 
 
-FILE = 'vectorinterp.py'
+FILE = 'extensive_field_converter.py'
 DOUBLE_ARRAY_PTR = numpy.ctypeslib.ndpointer(dtype=numpy.float64)
 
 
-class VectorInterp(object):
+class ExtensiveFieldConverter(object):
     """
-    A class to compute the vector representation of a 1-form in 2D
+    A class to convert intensive fields to extensive fields
     """
 
     def __init__(self):
@@ -19,14 +19,13 @@ class VectorInterp(object):
         Constructor.
         """
 
-        self.ptr = c_void_p()
-        self.obj = byref(self.ptr)
-        self.numTargetPoints = 0
         self.numGridEdges = 0
         self.numGridCells = 0
+        self.ptr = c_void_p()
+        self.obj = byref(self.ptr)
 
-        MINTLIB.mnt_vectorinterp_new.argtypes = [POINTER(c_void_p)]
-        ier = MINTLIB.mnt_vectorinterp_new(self.obj)
+        MINTLIB.mnt_extensivefieldconverter_new.argtypes = [POINTER(c_void_p)]
+        ier = MINTLIB.mnt_extensivefieldconverter_new(self.obj)
         if ier:
             error_handler(FILE, '__init__', ier)
 
@@ -34,8 +33,8 @@ class VectorInterp(object):
         """
         Destructor.
         """
-        MINTLIB.mnt_vectorinterp_del.argtypes = [POINTER(c_void_p)]
-        ier = MINTLIB.mnt_vectorinterp_del(self.obj)
+        MINTLIB.mnt_extensivefieldconverter_del.argtypes = [POINTER(c_void_p)]
+        ier = MINTLIB.mnt_extensivefieldconverter_del(self.obj)
         if ier:
             error_handler(FILE, '__del__', ier)
 
@@ -47,137 +46,89 @@ class VectorInterp(object):
         """
         self.numGridEdges = grid.getNumberOfEdges()
         self.numGridCells = grid.getNumberOfCells()
-        MINTLIB.mnt_vectorinterp_setGrid.argtypes = [POINTER(c_void_p), c_void_p]
-        ier = MINTLIB.mnt_vectorinterp_setGrid(self.obj, grid.ptr)
+        MINTLIB.mnt_extensivefieldconverter_setGrid.argtypes = [POINTER(c_void_p), c_void_p]
+        ier = MINTLIB.mnt_extensivefieldconverter_setGrid(self.obj, grid.ptr)
         if ier:
             error_handler(FILE, 'setGrid', ier)
 
-    def buildLocator(self, numCellsPerBucket=10, periodX=360., enableFolding=False):
+    def getEdgeData(self, vx, vy, placement):
         """
-        Build the cell locator.
-
-        :param numCellsPerBucket: approximate number of cells per bucket
-        :param periodX: periodicity in x (set to 0 if non-periodic)
-        :param enableFolding: whether (1) or not (0) |latitudes| > 90 should be folded back into the domain
-        :note: call this after setGrid
-        """
-        enableFoldingInt = 0
-        if enableFolding:
-            enableFoldingInt = 1
-
-        MINTLIB.mnt_vectorinterp_buildLocator.argtypes = [POINTER(c_void_p),
-                                                          c_int, c_double, c_int]
-        ier = MINTLIB.mnt_vectorinterp_buildLocator(self.obj,
-                                                    numCellsPerBucket, periodX, enableFoldingInt)
-        if ier:
-            error_handler(FILE, 'buildLocator', ier)
-
-    def findPoints(self, targetPoints, tol2=1.e-12):
-        """
-        Find the cells containing the target points.
-
-        :param targetPoints: array of size numPoints times 3
-        :param tol2: tolerance in the square of the distance
-        :returns the number of points outside the domain
-        """
-        if len(targetPoints.shape) != 2:
-            msg = 'ERROR: targetPoints should have dims (numTargetPoints, 3)'\
-                  f', got {targetPoints.shape}'
-            raise RuntimeError(msg)
-        if targetPoints.shape[-1] != 3:
-            msg = "ERROR: targetPoints' last dimension should be 3,"\
-                   f" got {targetPoints.shape[-1]}"
-            raise RuntimeError(msg)
-        MINTLIB.mnt_vectorinterp_findPoints.argtypes = [POINTER(c_void_p),
-                                                    c_size_t,
-                                                    DOUBLE_ARRAY_PTR,
-                                                    c_double]
-        self.numTargetPoints = targetPoints.shape[0]
-        numBad = MINTLIB.mnt_vectorinterp_findPoints(self.obj,
-                                                 self.numTargetPoints,
-                                                 targetPoints, tol2)
-        return numBad
-
-    def getEdgeVectors(self, data, placement=UNIQUE_EDGE_DATA):
-        """
-        Get the edge vectors at given target points.
-
-        :param data: edge line integrals (see placement argument below). The units
-                     of the line integrals must be consistent with the coordinates
-                     units (typically degrees). For instance, a velocity field
-                     in degrees/time expects a line integral in degrees^2/time.
-        :param placement: mint.CELL_BY_CELL_DATA if the data are cell by cell 
-                          (size num cells * mint.NUM_EDGES_PER_QUAD),
-                          assume unique edge Id data otherwise (size num edges)
-        :returns vector array of size numTargetPoints times 3
-        :note: call this after invoking findPoints.
+        Get the edge integrated data
+        :param vx: x component of vectors on edges, array of size numEdges
+        :param vy: y component of vectors on edges, array of size numEdges
+        :param placement: mint.CELL_BY_CELL_DATA if data are cell by cell (size num cells * mint.NUM_EDGES_PER_QUAD),
+                          data are assumed to be on unique edges otherwise (size num edges)
+        :returns edge integrated data, array size is numCells * mint.NUM_EDGES_PER_QUAD
+        :note: vx and vy should be compatible with the grid's coordinates. For instance, if vx and vy are 
+               velocities in m/s and the coordinates are in degrees then one needs transform 
+               vx -> vx*(180/pi)/(A*cos(lat*pi/180)) and vy -> vy*(180/pi)/A to get velocities in deg/sec
+               (A is the planet radius).
         """
 
         # check the data size
-        n = numpy.prod(data.shape)
-        if placement == CELL_BY_CELL_DATA and n != self.numGridCells * NUM_EDGES_PER_QUAD:
-            msg = f"data has wrong size (= {n}), num cells*NUM_EDGES_PER_QUAD = {self.numGridCells*NUM_EDGES_PER_QUAD}"
+        n0 = self.numGridCells * NUM_EDGES_PER_QUAD
+        if placement == CELL_BY_CELL_DATA and (vx.shape[0] != n0 or vy.shape[0] != n0):
+            msg = f"vx, vy have wrong size (= {vx.shape[0]}, {vy.shape[0]}), num cells*NUM_EDGES_PER_QUAD = {n0}"
             ier = 10
-            error_handler(FILE, 'getEdgeVectors', ier, detailedmsg=msg)
+            error_handler(FILE, 'getEdgeData', ier, detailedmsg=msg)
             return
-        elif placement != CELL_BY_CELL_DATA and n != self.numGridEdges:
-            msg = f"data has wrong size (= {n}), num edges = {self.numGridEdges}"
+        elif placement != CELL_BY_CELL_DATA and (vx.shape[0] != self.numGridEdges or vy.shape[0] != self.numGridEdges):
+            msg = f"vx, vy have wrong size (= {vx.shape[0]}, {vy.shape[0]}), num edges = {self.numGridEdges}"
             ier = 11
-            error_handler(FILE, 'getEdgeVectors', ier, detailedmsg=msg)
+            error_handler(FILE, 'getEdgeData', ier, detailedmsg=msg)
             return
 
-        MINTLIB.mnt_vectorinterp_getEdgeVectors.argtypes = [
+        res = numpy.empty(n0, numpy.float64)
+        MINTLIB.mnt_extensivefieldconverter_getEdgeData.argtypes = [
                                                         POINTER(c_void_p),
                                                         DOUBLE_ARRAY_PTR,
                                                         DOUBLE_ARRAY_PTR,
-                                                        c_int]
-        res = numpy.zeros((self.numTargetPoints, 3), numpy.float64)
-        ier = MINTLIB.mnt_vectorinterp_getEdgeVectors(self.obj, data, res,
-                                                      placement)
+                                                        c_int,
+                                                        DOUBLE_ARRAY_PTR]
+        ier = MINTLIB.mnt_extensivefieldconverter_getEdgeData(self.obj, vx, vy, placement, res)
         if ier:
-            msg = "Some target lines fall outside the grid."
-            warning_handler(FILE, 'getEdgeVectors', ier,
+            msg = "An error occurred after calling MINTLIB.mnt_extensivefieldconverter_getEdgeData."
+            warning_handler(FILE, 'getEdgeData', ier,
                             detailedmsg=msg)
         return res
 
-    def getFaceVectors(self, data, placement=UNIQUE_EDGE_DATA):
+    def getFaceData(self, vx, vy, placement):
         """
-        Get the lateral face vectors at given target points.
-
-        :param data: edge fluxes (see placement argument below). The units
-                     should be compatible with the coordinates units
-                     (typically degrees). For instance, a velocity field
-                     in degrees/time expects a flux in degrees^2/time.
-        :param placement: mint.CELL_BY_CELL_DATA if data are cell by cell 
-                          (size num cells * mint.NUM_EDGES_PER_QUAD),
-                          assume unique edge Id data otherwise (size num edges)
-        :returns vector array of size numTargetPoints times 3
-        :note: call this after invoking findPoints.
+        Get the face integrated data
+        :param vx: x component of vectors on edges, array of size numEdges
+        :param vy: y component of vectors on edges, array of size numEdges
+        :param placement: mint.CELL_BY_CELL_DATA if data are cell by cell (size num cells * mint.NUM_EDGES_PER_QUAD),
+                          data are assumed to be on unique edges otherwise (size num edges)
+        :returns face integrated data, array size is numCells * mint.NUM_EDGES_PER_QUAD
+        :note: vx and vy should be compatible with the grid's coordinates. For instance, if vx and vy are 
+               velocities in m/s and the coordinates are in degrees then one needs transform 
+               vx -> vx*(180/pi)/(A*cos(lat*pi/180)) and vy -> vy*(180/pi)/A to get velocities in deg/sec
+               (A is the planet radius).
         """
 
-        # check data size
-        n = numpy.prod(data.shape)
-        if placement == CELL_BY_CELL_DATA and n != self.numGridCells * NUM_EDGES_PER_QUAD:
-            msg = f"data has wrong size (= {n}), num cells*NUM_EDGES_PER_QUAD = {self.numGridCells*NUM_EDGES_PER_QUAD}"
+        # check the data size
+        n0 = self.numGridCells * NUM_EDGES_PER_QUAD
+        if placement == CELL_BY_CELL_DATA and (vx.shape[0] != n0 or vy.shape[0] != n0):
+            msg = f"vx, vy have wrong size (= {vx.shape[0]}, {vy.shape[0]}), num cells*NUM_EDGES_PER_QUAD = {n0}"
             ier = 10
-            error_handler(FILE, 'getEdgeVectors', ier, detailedmsg=msg)
+            error_handler(FILE, 'getEdgeData', ier, detailedmsg=msg)
             return
-        elif placement != CELL_BY_CELL_DATA and n != self.numGridEdges:
-            msg = f"data has wrong size (= {n}), num edges = {self.numGridEdges}"
+        elif placement != CELL_BY_CELL_DATA and (vx.shape[0] != self.numGridEdges or vy.shape[0] != self.numGridEdges):
+            msg = f"vx, vy have wrong size (= {vx.shape[0]}, {vy.shape[0]}), num edges = {self.numGridEdges}"
             ier = 11
-            error_handler(FILE, 'getEdgeVectors', ier, detailedmsg=msg)
+            error_handler(FILE, 'getFaceData', ier, detailedmsg=msg)
             return
 
-        MINTLIB.mnt_vectorinterp_getFaceVectors.argtypes = [
+        res = numpy.empty(n0, numpy.float64)
+        MINTLIB.mnt_extensivefieldconverter_getFaceData.argtypes = [
                                                         POINTER(c_void_p),
                                                         DOUBLE_ARRAY_PTR,
                                                         DOUBLE_ARRAY_PTR,
-                                                        c_int]
-        res = numpy.zeros((self.numTargetPoints, 3), numpy.float64)
-        ier = MINTLIB.mnt_vectorinterp_getFaceVectors(self.obj, data, res,
-                                                      placement)
+                                                        c_int,
+                                                        DOUBLE_ARRAY_PTR]
+        ier = MINTLIB.mnt_extensivefieldconverter_getFaceData(self.obj, vx, vy, placement, res)
         if ier:
-            msg = "Some target lines fall outside the grid."
-            warning_handler(FILE, 'getFaceVectors', ier,
+            msg = "An error occurred after calling MINTLIB.mnt_extensivefieldconverter_getFaceData."
+            warning_handler(FILE, 'getFaceData', ier,
                             detailedmsg=msg)
         return res
