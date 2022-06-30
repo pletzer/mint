de3a6e80053f618d30909a9e4498d1c212ba850a
Alex Pletzer <alexander@gokliya.net>
2022-07-01 08:34:49 +1200

    + note regarding the velocity transformation

--- src/mntExtensiveFieldConverter.h
+++ src/mntExtensiveFieldConverter.h
@@ -47,6 +47,10 @@ int mnt_extensivefieldconverter_setGrid(ExtensiveFieldConverter_t** self, Grid_t
  * @param vy y component of vectors on edges, array of size numCells*MNT_NUM_EDGES_PER_QUAD
  * @param data edge integrated data, array size is numCells * MNT_NUM_EDGES_PER_QUAD
  * @return error code (0 = OK)
+ * @note vx and vy should be compatible with the grid's coordinates. For instance, if vx and vy are 
+ *       velocities in m/s and the coordinates are in degrees then one needs transform 
+ *       vx -> vx*(180/pi)/(A*cos(lat*pi/180)) and vy -> vy*(180/pi)/A to get velocities in deg/sec
+ *       (A is the planet radius).
  */
 LIBRARY_API
 int mnt_extensivefieldconverter_getEdgeDataFromCellByCellVectors(ExtensiveFieldConverter_t** self,
@@ -60,6 +64,10 @@ int mnt_extensivefieldconverter_getEdgeDataFromCellByCellVectors(ExtensiveFieldC
  * @param vy y component of vectors on faces, array of size numCells*MNT_NUM_EDGES_PER_QUAD
  * @param data edge integrated data, array size is numCells * MNT_NUM_EDGES_PER_QUAD
  * @return error code (0 = OK)
+ * @note vx and vy should be compatible with the grid's coordinates. For instance, if vx and vy are 
+ *       velocities in m/s and the coordinates are in degrees then one needs transform 
+ *       vx -> vx*(180/pi)/(A*cos(lat*pi/180)) and vy -> vy*(180/pi)/A to get velocities in deg/sec
+ *       (A is the planet radius).
  */
 LIBRARY_API
 int mnt_extensivefieldconverter_getFaceDataFromCellByCellVectors(ExtensiveFieldConverter_t** self,
@@ -73,6 +81,10 @@ int mnt_extensivefieldconverter_getFaceDataFromCellByCellVectors(ExtensiveFieldC
  * @param vy y component of vectors on edges, array of size numEdges
  * @param data edge integrated data, array size is numCells * MNT_NUM_EDGES_PER_QUAD
  * @return error code (0 = OK)
+ * @note vx and vy should be compatible with the grid's coordinates. For instance, if vx and vy are 
+ *       velocities in m/s and the coordinates are in degrees then one needs transform 
+ *       vx -> vx*(180/pi)/(A*cos(lat*pi/180)) and vy -> vy*(180/pi)/A to get velocities in deg/sec
+ *       (A is the planet radius).
  */
 LIBRARY_API
 int mnt_extensivefieldconverter_getEdgeDataFromUniqueEdgeVectors(ExtensiveFieldConverter_t** self,
@@ -86,6 +98,10 @@ int mnt_extensivefieldconverter_getEdgeDataFromUniqueEdgeVectors(ExtensiveFieldC
  * @param vy y component of vectors on faces, array of size numEdges
  * @param data edge integrated data, array size is numCells * MNT_NUM_EDGES_PER_QUAD
  * @return error code (0 = OK)
+ * @note vx and vy should be compatible with the grid's coordinates. For instance, if vx and vy are 
+ *       velocities in m/s and the coordinates are in degrees then one needs transform 
+ *       vx -> vx*(180/pi)/(A*cos(lat*pi/180)) and vy -> vy*(180/pi)/A to get velocities in deg/sec
+ *       (A is the planet radius).
  */
 LIBRARY_API
 int mnt_extensivefieldconverter_getFaceDataFromUniqueEdgeVectors(ExtensiveFieldConverter_t** self,
@@ -102,6 +118,10 @@ int mnt_extensivefieldconverter_getFaceDataFromUniqueEdgeVectors(ExtensiveFieldC
  *                  otherwise (size num edges)
  * @param data edge integrated data, array size is numCells * MNT_NUM_EDGES_PER_QUAD
  * @return error code (0 = OK)
+ * @note vx and vy should be compatible with the grid's coordinates. For instance, if vx and vy are 
+ *       velocities in m/s and the coordinates are in degrees then one needs transform 
+ *       vx -> vx*(180/pi)/(A*cos(lat*pi/180)) and vy -> vy*(180/pi)/A to get velocities in deg/sec
+ *       (A is the planet radius).
  */
 LIBRARY_API
 int mnt_extensivefieldconverter_getEdgeData(ExtensiveFieldConverter_t** self,
@@ -118,6 +138,10 @@ int mnt_extensivefieldconverter_getEdgeData(ExtensiveFieldConverter_t** self,
  *                  otherwise (size num edges)
  * @param data edge integrated data, array size is numCells * MNT_NUM_EDGES_PER_QUAD
  * @return error code (0 = OK)
+ * @note vx and vy should be compatible with the grid's coordinates. For instance, if vx and vy are 
+ *       velocities in m/s and the coordinates are in degrees then one needs transform 
+ *       vx -> vx*(180/pi)/(A*cos(lat*pi/180)) and vy -> vy*(180/pi)/A to get velocities in deg/sec
+ *       (A is the planet radius).
  */
 LIBRARY_API
 int mnt_extensivefieldconverter_getFaceData(ExtensiveFieldConverter_t** self,
