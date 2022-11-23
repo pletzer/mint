6a352f310cd63556c6e59356e8f5666f8eff803c
Alex Pletzer <alexander@gokliya.net>
2022-11-24 12:01:45 +1300

    added cartesian computation of fluxes, tested for an elongated cell

--- src/mntExtensiveFieldAdaptor.cpp
+++ src/mntExtensiveFieldAdaptor.cpp
@@ -159,7 +159,7 @@ int mnt_extensivefieldadaptor__fromVectorFieldEdgeUniqueIdData(ExtensiveFieldAda
             double cosTheDx = cosTheta * dx;
 
             // edge integral
-            data[edgeId] = edgeSign * ( u[edgeId] * cosTheDx + v[edgeId] * dy );
+            data[edgeId] = edgeSign * (u[edgeId]*cosTheDx + v[edgeId]*dy);
         }
     }
 
@@ -215,6 +215,7 @@ int mnt_extensivefieldadaptor__fromVectorFieldFaceUniqueIdData(ExtensiveFieldAda
             double cosTheta = cos( deg2rad * 0.5 * (y1 + y0) );
             double cosTheDx = cosTheta * dx;
 
+
             // flux integral
 
             // our convention is to have the extensive fluxes pointing in the positive, logical direction.
@@ -222,6 +223,8 @@ int mnt_extensivefieldadaptor__fromVectorFieldFaceUniqueIdData(ExtensiveFieldAda
             double sign = -2*((iedge + 1) % 2) + 1;
 
             data[edgeId] = edgeSign*sign*(u[edgeId]*dy - v[edgeId]*cosTheDx);
+
+            // ADD CARTESIAN TO DO 
         }
     }
 
@@ -237,7 +240,7 @@ int mnt_extensivefieldadaptor__fromVectorFieldEdgeCellByCellData(ExtensiveFieldA
 
     double coef = 0;
     if ((*self)->grid->degrees) {
-        coef = M_PI/180.0;
+        coef = 1.0;
     }
     double deg2rad = M_PI / 180.;
 
@@ -259,10 +262,10 @@ int mnt_extensivefieldadaptor__fromVectorFieldEdgeCellByCellData(ExtensiveFieldA
             // length on the surface of the sphere (1 if no spherical)
             double cosTheDx = cos( coef * 0.5 * (y1 + y0) ) * dx;
 
-            double len_sq = cosTheDx * cosTheDx + dy * dy;
-
             std::size_t k = icell*MNT_NUM_EDGES_PER_QUAD + ie;
             data[k] = u[k]*cosTheDx + v[k]*dy;
+
+            // ADD CARTESIAN COMPUTATIOON AND SELECT (TO DO)
         }
     }
 
@@ -278,7 +281,7 @@ int mnt_extensivefieldadaptor__fromVectorFieldFaceCellByCellData(ExtensiveFieldA
 
     double coef = 0;
     if ((*self)->grid->degrees) {
-        coef = M_PI/180.0;
+        coef = 1.0;
     }
     double deg2rad = M_PI / 180.;
 
@@ -287,23 +290,64 @@ int mnt_extensivefieldadaptor__fromVectorFieldFaceCellByCellData(ExtensiveFieldA
     for (vtkIdType icell = 0; icell < (vtkIdType) (*self)->numCells; ++icell) {
         for (int ie = 0; ie < MNT_NUM_EDGES_PER_QUAD; ++ie) {
 
+            std::size_t k = icell*MNT_NUM_EDGES_PER_QUAD + ie;
+
             ier = mnt_grid_getPoints(&(*self)->grid, icell, ie, point0, point1);
             numFailures += ier;
 
-            double x0 = point0[LON_INDEX] * deg2rad;
-            double x1 = point1[LON_INDEX] * deg2rad;
-            double dx = x1 - x0;
-            double y0 = point0[LAT_INDEX] * deg2rad;
-            double y1 = point1[LAT_INDEX] * deg2rad;
-            double dy = y1 - y0;
+            double lam0 = point0[LON_INDEX] * deg2rad;
+            double lam1 = point1[LON_INDEX] * deg2rad;
+            double lamMid = 0.5*(lam0 + lam1);
 
-            // length on the surface of the sphere (1 if no spherical)
-            double cosTheDx = cos( coef * 0.5 * (y1 + y0) ) * dx;
+            double the0 = point0[LAT_INDEX] * deg2rad;
+            double the1 = point1[LAT_INDEX] * deg2rad;
+            double theMid = 0.5*(the0 + the1);
+           
+            double uval = u[k];
+            double vval = v[k];
 
-            double len_sq = cosTheDx * cosTheDx + dy * dy;
+            double a = 1.0;
+            double cosLam = cos(lamMid);
+            double sinLam = sin(lamMid);
+            double cosThe = cos(theMid);
+            double sinThe = sin(theMid);
 
-            std::size_t k = icell*MNT_NUM_EDGES_PER_QUAD + ie;
-            data[k] = u[k]*dy - v[k]*cosTheDx;
+            // Cartesian estimate
+            double zMid = a*sinThe;
+            double rhoMid = a*cosThe;
+            double xMid = rhoMid*cosLam;
+            double yMid = rhoMid*sinLam;
+
+            double vx = - uval*sinLam - vval*zMid*cosLam/a;
+            double vy = + uval*cosLam - vval*zMid*sinLam/a;
+            double vz = vval*rhoMid/a;
+
+            double rho0 = a*cos(the0);
+            double rho1 = a*cos(the1);
+            double dx = rho1*cos(lam1) - rho0*cos(lam0);
+            double dy = rho1*sin(lam1) - rho0*sin(lam0);
+            double dz = a*(sin(the1) - sin(the0));
+
+            double dataXYZ = (dy*zMid - dz*yMid)*vx/a
+                           + (dz*xMid - dx*zMid)*vy/a
+                           + (dx*yMid - dy*xMid)*vz/a;
+
+            // estimate in spherical coordinates
+            double cosTheDLam = cosThe*(lam1 - lam0);
+            double dThe = the1 - the0;
+            double dataSph = u[k]*dThe - v[k]*cosTheDLam;
+
+            // choose the spherical estimate except when one of the points is at the pole
+            const double eps = 10 * std::numeric_limits<double>::epsilon();
+            if ((*self)->grid->degrees && 
+                (std::fabs(std::fabs(point0[LAT_INDEX]) - 90.) < eps || std::fabs(std::fabs(point1[LAT_INDEX]) - 90.) < eps)) {
+                // use estimate obtained from Cartesian coordinates when one of the points is at the poles
+                data[k] = dataXYZ;
+            }
+            else {
+                // use estimate from spherical coordinates
+                data[k] = dataSph;
+            }
         }
     }
 
