66efa722796eff892b04b221e190ec85b7557163
Alex Pletzer <alexander@gokliya.net>
2023-02-27 14:00:55 +1300

    added an open flux test

--- mint/tests/test_iris_flux.py
+++ mint/tests/test_iris_flux.py
@@ -23,5 +23,27 @@ def test_cs_zt():
     assert np.all(np.fabs(flux - 0.0) < 1.e-10)
 
 
+def test_cs_zt2():    
+
+    src_u, src_v = _u_v_cubes_from_ugrid_file(DATA_DIR / 'cs128_wind_zt.nc')
+
+    # w2
+    _set_vector_field_from_streamfct(src_u, src_v)
+
+    # grid options for a cubed-sphere grid
+    src_flags = (1, 1, 1)
+
+    xybeg = np.array([0., 0.])
+    xyend = np.array([256., 72.])
+    xy = [xybeg, xyend]
+    flx_calc = mint.IrisMintFlux(src_u.mesh, src_flags=src_flags, tgt_line=xy)
+    flux = flx_calc.evaluate_from_vector(src_u, src_v, fs=mint.FUNC_SPACE_W2)
+
+    # check
+    sbeg = np.cos(xybeg[0]*np.pi/180.)*np.cos(xybeg[1]*np.pi/180.)
+    send = np.cos(xyend[0]*np.pi/180.)*np.cos(xyend[1]*np.pi/180.)
+    exact = send - sbeg
+    assert np.all(np.fabs(flux - exact) < 6.e-5)
+
 
 
