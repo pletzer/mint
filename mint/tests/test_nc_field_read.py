6a68772fbc9d584196834c9c2e29f4234fb36a4e
Alex Pletzer <alexander@gokliya.net>
2023-03-09 11:55:54 +1300

    now nc_field_read test is passing

--- mint/tests/test_nc_field_read.py
+++ mint/tests/test_nc_field_read.py
@@ -8,7 +8,7 @@ def test_1():
     nf = mint.NcFieldRead(DATA_DIR / 'cs8_wind.nc', 'u_in_w2h')
     assert nf.getNumDims() == 1
     nedges = nf.getDim(iAxis=0)
-    #assert nf.getDimName(iAxis=0) == 'ncs_edge'
+    assert nf.getDimName(iAxis=0) == 'ncs_edge'
     data = np.empty((nedges,), np.float64)
     nf.data(data)
     print(data)
