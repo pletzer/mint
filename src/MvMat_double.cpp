c44a532a40964ec588253c92efa5777b0c0f3be4
Alexander Pletzer <alexander@gokliya.net>
2026-08-21 21:27:51 +0200

    fixed compilation warnings

--- src/MvMat_double.cpp
+++ src/MvMat_double.cpp
@@ -29,41 +29,37 @@ ColMat<double> eye(std::size_t n){
     
 ***********************************************************************/
 
-ColMat<double> load(const std::string& cfile) { 
+ColMat<double> load(const std::string& cfile) {
 
-  // copy to temp1 
+  // strip lines starting with % (comments), in memory -- no temp files, no shelling
+  // out to cp/sed/rm (which also meant cfile was being pasted, unescaped, into a shell
+  // command)
+  ifstream file(cfile.c_str());
+  std::ostringstream stripped;
+  std::string line;
+  while (std::getline(file, line)) {
+    if (line.empty() || line[0] != '%') {
+      stripped << line << '\n';
+    }
+  }
 
-  std::string temp1 = "temp1"; 
-  std::string s1 = "cp " + cfile + " " + temp1 + '\n'; 
-  system(s1.c_str()); 
+  std::istringstream in(stripped.str());
 
-  // get rid of lines starting with % (comments) 
-
-  std::string temp2 = "temp2"; 
-  std::string s2 = "sed 's;^%.*$;;g' " + temp1 + " > " + temp2 + '\n'; 
-  system(s2.c_str()); 
-  
-  const char *c_temp2 = temp2.c_str(); 
-  ifstream file(c_temp2); 
-
-  unsigned int nr, nc; 
-  file >> nr; 
-  file >> nc; 
+  unsigned int nr, nc;
+  in >> nr;
+  in >> nc;
 
 
-  ColMat<double> a(nr, nc, 0.); 
+  ColMat<double> a(nr, nc, 0.);
 
-  for(unsigned int i = 0; i < nr; ++i){ 
-    for(unsigned int j = 0; j < nc; ++j){ 
-      file >> a(i,j); 
-    } 
-  } 
+  for(unsigned int i = 0; i < nr; ++i){
+    for(unsigned int j = 0; j < nc; ++j){
+      in >> a(i,j);
+    }
+  }
 
-  std::string s3 = "rm " + temp1 + ' ' + temp2 + '\n'; 
-  system(s3.c_str()); 
-
-  return a; 
-} 
+  return a;
+}
 
 // real matrix dot complex vector
 Vec_cmplx dot(const Mat &a, const Vec_cmplx &b)
